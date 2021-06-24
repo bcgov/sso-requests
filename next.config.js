df6eb2ff73a5d29a33b690450ec7fc6e84ea0cf6
Jonathan Langlois <jon@button.is>
2021-06-24 09:21:54 -0700

    lint boilerplate files
    run linter on code of conduct, readme, next.config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   reactStrictMode: true,
   // For gh pages deployment need to prefix the repository as part of the base url
-  assetPrefix: process.env.NODE_ENV === "development" ?  '' : '/sso-requests/',
+  assetPrefix: process.env.NODE_ENV === 'development' ? '' : '/sso-requests/',
 };
