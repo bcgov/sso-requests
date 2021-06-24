e1f481660fab117008a7bb123c95c9a9c3ce37e1
Jonathan Langlois <jon@button.is>
2021-06-24 11:31:45 -0700

    chore: formatting
    add specific ubuntu version, consistent actions format, homepage in package.json

--- app/next.config.js
+++ app/next.config.js
@@ -1,5 +1,3 @@
 module.exports = {
   reactStrictMode: true,
-  // For gh pages deployment need to prefix the repository as part of the base url
-  assetPrefix: process.env.NODE_ENV === 'development' ? '' : '/sso-requests/',
 };
