1fe798ef9b657f5c1a4658ab0e417866f2a12148
Jonathan Langlois <jon@button.is>
2021-06-30 10:35:48 -0700

    chore: rename folders
    rename lambda function folders to more generic terms

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -19,7 +19,7 @@ EOF
 }
 
 resource "aws_lambda_function" "auth" {
-  filename      = "router.zip"
+  filename      = "app.zip"
   function_name = "handler"
   role          = aws_iam_role.iam_for_lambda.arn
   # has to have the form filename.functionname where filename is the file containing the export
@@ -28,7 +28,7 @@ resource "aws_lambda_function" "auth" {
   # The filebase64sha256() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
   # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
-  source_code_hash = filebase64sha256("router.zip")
+  source_code_hash = filebase64sha256("app.zip")
 
   runtime = "nodejs12.x"
 
