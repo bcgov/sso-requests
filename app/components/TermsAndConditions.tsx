dada14699df52559ba14eeca36640f5b8ec5b968
Jonathan Langlois <jon@button.is>
2021-07-19 16:17:12 -0700

    fix: escape
    escape quotations from text contents'

--- app/components/TermsAndConditions.tsx
+++ app/components/TermsAndConditions.tsx
@@ -6,13 +6,13 @@ const Title = styled.h2`
 function Termsandconditions() {
   return (
     <>
-      <Title>We're a Community</Title>
+      <Title>We&apos;re a Community</Title>
       <ul>
         <li>
           As part of a community, we can solve things together and quickly. Please join the #SSO channel on Rocket.Chat
         </li>
         <li>Coordinate load testing with the Pathfinder SSO Team, and please only use the Test environment</li>
-        <li>The SSO Service is multitenancy, please keep request load levels within "x" requests/second</li>
+        <li>The SSO Service is multitenancy, please keep request load levels within &quot;x&quot; requests/second</li>
         <li>
           Please follow the BC Government digital standards (
           <a href="https://digital.gov.bc.ca/resources/digital-principles">link</a>)
@@ -21,13 +21,13 @@ function Termsandconditions() {
       <Title>Understanding the service level</Title>
       <ul>
         <li>
-          The SSO service level is "best-effort" during business hours (Monday to Friday, 9am to 5pm), and after-hours
-          resolution time is not guaranteed
+          The SSO service level is &quot;best-effort&quot; during business hours (Monday to Friday, 9am to 5pm), and
+          after-hours resolution time is not guaranteed
         </li>
-        <li>Please take this into account if you have a "critical" application</li>
+        <li>Please take this into account if you have a &quot;critical&quot; application</li>
         <li>From time to time, we may contact you to confirm if your configuration is needed</li>
       </ul>
-      <Title>What's included in the service</Title>
+      <Title>What&apos;s included in the service</Title>
       <ul>
         <li>
           The Standard realm is configured to meet the general needs of the community. Currently, custom configurations,
