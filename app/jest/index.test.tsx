c17f76565620da646448873fb5306dd94ef50dd1
Junmin Ahn <junminahn@outlook.com>
2021-07-20 21:57:56 -0700

    chore: fix unit tests

--- app/jest/index.test.tsx
+++ app/jest/index.test.tsx
@@ -5,7 +5,7 @@ import HomePage from 'pages/index';
 describe('HomePage', () => {
   it('should render the card heading', () => {
     render(<HomePage currentUser={null} />);
-    const login = screen.getByText('Keycloak Vision');
+    const login = screen.getByText('Pathfinder SSO Vision');
     expect(login).toBeInTheDocument();
   });
 });
