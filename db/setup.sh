6adf47ed6ef2baa39e034d68530b12a9e838c3bb
Junmin Ahn <junminahn@outlook.com>
2021-07-26 14:16:28 -0700

    chore: use another db name in ci

--- db/setup.sh
+++ db/setup.sh
@@ -2,7 +2,7 @@
 if [[ -n "$1" ]]; then
  db="$1"
 else
- db="foi"
+ db="mydb"
 fi
 
 echo "SELECT 'CREATE DATABASE $db' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '$db')\gexec" | psql -d postgres
