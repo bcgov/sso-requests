797926d031ba3983c9531610a030159854cfa4d4
Jonathan Langlois <jon@button.is>
2021-08-09 10:41:13 -0700

    chore: run make lambda
    re-run terraform when there are changes to schemas the lambda functions share

--- app/schemas/non-bceid-schemas.ts
+++ app/schemas/non-bceid-schemas.ts
@@ -1,6 +1,6 @@
-import requesterInfoSchema from 'schemas/requester-info';
-import termsAndConditionsSchema from 'schemas/terms-and-conditions';
-import providersSchema from 'schemas/providers';
+import requesterInfoSchema from 'schemas/shared/requester-info';
+import termsAndConditionsSchema from 'schemas/shared/terms-and-conditions';
+import providersSchema from 'schemas/shared/providers';
 
 const nonBceidSchemas = [requesterInfoSchema, providersSchema, termsAndConditionsSchema];
 export default nonBceidSchemas;
