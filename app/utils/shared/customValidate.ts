d82effe38ef7efe6083f54f5caf73856dbf4221c
Jonathan Langlois <jon@button.is>
2021-08-10 12:37:13 -0700

    fix: trim
    make sure uri doesnt have trailing endspace

--- app/utils/shared/customValidate.ts
+++ app/utils/shared/customValidate.ts
@@ -1,22 +1,25 @@
 const isValidKeycloakURI = (uri: string) => {
   try {
     const url = new URL(uri);
+    if (uri !== uri.trim()) return false;
     return true;
   } catch (err) {
     return false;
   }
 };
 
+const validationMessage = 'Please enter a valid URI, including an http:// or https:// prefix';
+
 export const customValidate = (formData: any, errors: any) => {
   const { devValidRedirectUris = [], testValidRedirectUris = [], prodValidRedirectUris = [] } = formData;
   let isAllValid = devValidRedirectUris.every(isValidKeycloakURI);
-  if (!isAllValid) errors.devValidRedirectUris.addError('Please enter a valid URI');
+  if (!isAllValid) errors.devValidRedirectUris.addError(validationMessage);
 
   isAllValid = testValidRedirectUris.every(isValidKeycloakURI);
-  if (!isAllValid) errors.testValidRedirectUris.addError('Please enter a valid URI');
+  if (!isAllValid) errors.testValidRedirectUris.addError(validationMessage);
 
   isAllValid = prodValidRedirectUris.every(isValidKeycloakURI);
-  if (!isAllValid) errors.prodValidRedirectUris.addError('Please enter a valid URI');
+  if (!isAllValid) errors.prodValidRedirectUris.addError(validationMessage);
 
   return errors;
 };
