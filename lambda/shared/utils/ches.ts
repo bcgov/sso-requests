e7b4be7a80ce53b34d93ceb81bbe22241925ac47
Jonathan Langlois <jon@button.is>
2021-08-16 15:46:21 -0700

    chore: email
    move email function to shared utils, send messages on app updates

--- lambda/shared/utils/ches.ts
+++ lambda/shared/utils/ches.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { EmailOptions } from './interfaces';
+import { EmailOptions } from '../interfaces';
 const url = require('url');
 
 const fetchChesToken = async (username, password) => {
