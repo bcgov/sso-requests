ca08c184bb1ef26cf360e5e13e40beb3dc51efd7
Jonathan Langlois <jon@button.is>
2021-08-19 15:25:58 -0700

    chore: error message
    display error codes with message

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -73,6 +73,14 @@ function MyApp({ Component, pageProps }: AppProps) {
         removeTokens();
         setCurrentUser(null);
         setLoading(false);
+        if (tokens.id_token) {
+          router.push({
+            pathname: 'application-error',
+            query: {
+              error: 'E02',
+            },
+          });
+        }
       }
     }
 
--- app/pages/application-error.tsx
+++ app/pages/application-error.tsx
@@ -1,10 +1,18 @@
 import styled from 'styled-components';
-import errorImage from 'svg/error';
+import ErrorImage from 'svg/error';
+import { useRouter } from 'next/router';
 
 const Container = styled.div`
   text-align: center;
 `;
 
 export default function ApplicationError() {
-  return <Container>{errorImage}</Container>;
+  const router = useRouter();
+  const errorMessage = router?.query?.error as string;
+
+  return (
+    <Container>
+      <ErrorImage message={errorMessage} />
+    </Container>
+  );
 }
