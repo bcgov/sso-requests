7b228aac3550b2fdef2e62b84d2ab7ade43b014f
Jonathan Langlois <jon@button.is>
2021-09-01 15:22:49 -0700

    fix: test
    fix test for duplicate code

--- lambda/app/src/__tests__/getDifferences.test.ts
+++ lambda/app/src/__tests__/getDifferences.test.ts
@@ -1,50 +1,5 @@
 import { getDifferences } from '../utils/helpers';
-
-const formDataUpdated: any = {
-  id: 1,
-  idirUserid: 'TEST',
-  projectName: 'test',
-  clientName: 'test',
-  realm: 'onestopauth',
-  publicAccess: true,
-  devValidRedirectUris: ['https://b'],
-  testValidRedirectUris: ['https://a'],
-  prodValidRedirectUris: ['https://a'],
-  environments: ['dev', 'test', 'prod'],
-  prNumber: 10,
-  actionNumber: 10,
-  createdAt: 'test',
-  updatedAt: '10-11-2012',
-  projectLead: false,
-  preferredEmail: 'test',
-  newToSso: true,
-  agreeWithTerms: true,
-  status: 'draft',
-  archived: false,
-};
-
-const formDataOriginal: any = {
-  id: 1,
-  idirUserid: 'TEST',
-  projectName: 'test',
-  clientName: 'test',
-  realm: 'onestopauth',
-  publicAccess: true,
-  devValidRedirectUris: ['https://a'],
-  testValidRedirectUris: ['https://a'],
-  prodValidRedirectUris: ['https://a'],
-  environments: ['dev', 'test', 'prod'],
-  prNumber: 10,
-  actionNumber: 10,
-  createdAt: 'test',
-  updatedAt: '11-12-2013',
-  projectLead: false,
-  preferredEmail: 'test',
-  newToSso: true,
-  agreeWithTerms: true,
-  status: 'draft',
-  archived: false,
-};
+import { formDataOriginal, formDataUpdated } from './fixtures';
 
 const expectedDiff = [{ kind: 'E', lhs: 'https://a', path: ['devValidRedirectUris', 0], rhs: 'https://b' }];
 
