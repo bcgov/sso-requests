063056f3661807ce543c61987a798a90123b6404
Jonathan Langlois <jon@button.is>
2021-09-02 15:53:31 -0700

    fix: test
    swap order in test as well

--- lambda/app/src/__tests__/getDifferences.test.ts
+++ lambda/app/src/__tests__/getDifferences.test.ts
@@ -1,7 +1,7 @@
 import { getDifferences } from '../utils/helpers';
 import { formDataOriginal, formDataUpdated } from './fixtures';
 
-const expectedDiff = [{ kind: 'E', lhs: 'https://a', path: ['devValidRedirectUris', 0], rhs: 'https://b' }];
+const expectedDiff = [{ kind: 'E', lhs: 'https://b', path: ['devValidRedirectUris', 0], rhs: 'https://a' }];
 
 it('should display the differences', () => {
   expect(getDifferences(formDataOriginal, formDataUpdated)).toEqual(expectedDiff);
