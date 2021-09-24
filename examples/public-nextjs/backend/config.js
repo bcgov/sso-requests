8aa223b52de7946883a05305dcaae791fac59f06
Jonathan Langlois <jon@button.is>
2021-09-24 08:47:18 -0700

    feat: example
    example express app with confidential client

--- examples/public-nextjs/backend/config.js
+++ examples/public-nextjs/backend/config.js
@@ -8,7 +8,7 @@ let keycloak = new Keycloak({ store: memoryStore }, kcConfig);
 
 module.exports = {
   HOSTNAME: process.env.HOST || '0.0.0.0',
-  PORT: process.env.PORT || 3000,
+  PORT: process.env.PORT || 8080,
   keycloak,
   COOKIE_SESSION_SECRET: 'superSecretValue',
   memoryStore,
