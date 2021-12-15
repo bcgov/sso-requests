cab9951c2639ee641d59ead1450ba6ed3ca0e170
Jonathan Langlois <jon@button.is>
2021-12-15 12:43:10 -0800

    fix: sonar
    fix sonarcloud duplicated code warnings

--- app/schemas/shared/requester-info.ts
+++ app/schemas/shared/requester-info.ts
@@ -1,3 +1,25 @@
+const projectLead = {
+  type: 'boolean',
+  title: 'Are you the product owner or technical contact for this project?',
+  tooltipContent: `
+<p>
+  Only the person who is responsible for this project should be submitting the integration request. 
+  If you are not the one accountable, please refer this request to a team member who will be accountable for this project.
+</p>`,
+};
+
+const projectName = { type: 'string', title: 'Project Name', maxLength: 50 };
+const preferredEmail = { type: 'string', title: 'Default Email Address', format: 'email', maxLength: 250 };
+const additionalEmails = {
+  type: ['array', 'null'],
+  title: 'Additional Emails',
+  items: { type: 'string', maxLength: 250, format: 'email' },
+  additionalItems: { type: 'string', maxLength: 250, format: 'email' },
+  deletableIndex: 0,
+  maxItems: 2,
+  addItemText: 'Add Email Address',
+};
+
 export default function getSchema(hasTeam = true) {
   return {
     type: 'object',
@@ -15,15 +37,7 @@ export default function getSchema(hasTeam = true) {
           {
             properties: {
               usesTeam: { enum: [false] },
-              projectLead: {
-                type: 'boolean',
-                title: 'Are you the product owner or technical contact for this project?',
-                tooltipContent: `
-              <p>
-                Only the person who is responsible for this project should be submitting the integration request. 
-                If you are not the one accountable, please refer this request to a team member who will be accountable for this project.
-              </p>`,
-              },
+              projectLead,
             },
           },
           {
@@ -36,17 +50,9 @@ export default function getSchema(hasTeam = true) {
                   enum: ['one', 'two'],
                 },
               }),
-              projectName: { type: 'string', title: 'Project Name', maxLength: 50 },
-              preferredEmail: { type: 'string', title: 'Default Email Address', format: 'email', maxLength: 250 },
-              additionalEmails: {
-                type: ['array', 'null'],
-                title: 'Additional Emails',
-                items: { type: 'string', maxLength: 250, format: 'email' },
-                additionalItems: { type: 'string', maxLength: 250, format: 'email' },
-                deletableIndex: 0,
-                maxItems: 2,
-                addItemText: 'Add Email Address',
-              },
+              projectName,
+              preferredEmail,
+              additionalEmails,
             },
           },
         ],
@@ -56,17 +62,9 @@ export default function getSchema(hasTeam = true) {
           {
             properties: {
               projectLead: { enum: [true] },
-              projectName: { type: 'string', title: 'Project Name', maxLength: 50 },
-              preferredEmail: { type: 'string', title: 'Default Email Address', format: 'email', maxLength: 250 },
-              additionalEmails: {
-                type: ['array', 'null'],
-                title: 'Additional Emails',
-                items: { type: 'string', maxLength: 250, format: 'email' },
-                additionalItems: { type: 'string', maxLength: 250, format: 'email' },
-                deletableIndex: 0,
-                maxItems: 2,
-                addItemText: 'Add Email Address',
-              },
+              projectName,
+              preferredEmail,
+              additionalEmails,
             },
           },
         ],
