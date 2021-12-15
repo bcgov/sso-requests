465454a969e75dd2140a60ef38c6759429b7aa64
jlangy <37274633+jlangy@users.noreply.github.com>
2021-12-15 18:11:02 +0000

    Deploying to gh-pages from @ bcgov/sso-requests@4636c4c6377ddbb57d949c28074add774a965a32 🚀

--- _next/static/chunks/pages/my-requests-f4c530893c2924ef7a9d.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[12],{66810:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/my-requests",function(){return u(85681)}])}},function(n){n.O(0,[294,962,356,483,774,888,179],(function(){return _=66810,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
