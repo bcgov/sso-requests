99d72c3f2fa5e5caba822c21ed47a5f3b813ad05
Jonathan Langlois <jon@button.is>
2022-01-05 12:23:46 -0800

    chore: update
    migration to change null uses_team values to false

--- lambda/db/src/migrations/2021.12.18T15.57.91.update-requests-table.ts
+++ lambda/db/src/migrations/2021.12.18T15.57.91.update-requests-table.ts
@@ -1,7 +1,8 @@
-const { DataTypes } = require('sequelize');
+const { Op, DataTypes } = require('sequelize');
 
 // see https://sequelize.org/master/manual/naming-strategies.html
 export const up = async ({ context: sequelize }) => {
+  await sequelize.getQueryInterface().bulkUpdate('requests', { uses_team: false }, { uses_team: { [Op.eq]: null } });
   await sequelize.getQueryInterface().changeColumn('requests', 'uses_team', {
     type: DataTypes.BOOLEAN,
     allowNull: false,
