afd9275bfff7bc377d456c8f680237929ac2a56e
Junmin Ahn <junminahn@outlook.com>
2022-02-02 14:09:30 -0800

    fix: checks team members to allow json installation download

--- lambda/app/src/controllers/installation.ts
+++ lambda/app/src/controllers/installation.ts
@@ -1,13 +1,27 @@
-import { models } from '../../../shared/sequelize/models/models';
-import { Session } from '../../../shared/interfaces';
+import { Op } from 'sequelize';
+import { sequelize, models } from '../../../shared/sequelize/models/models';
+import { Session, User } from '../../../shared/interfaces';
 import { generateInstallation, updateClientSecret } from '../keycloak/installation';
 
-export const getInstallation = async (session: Session, data: { requestId: number; environment: string }) => {
-  const request = await models.request.findOne({
-    where: {
-      idirUserid: session.idir_userid,
-      id: data.requestId,
+export const getInstallation = async (
+  session: Session,
+  user: User,
+  data: { requestId: number; environment: string },
+) => {
+  const where: any = { id: data.requestId };
+
+  where[Op.or] = [
+    {
+      usesTeam: true,
+      teamId: { [Op.in]: sequelize.literal(`(select team_id from users_teams where user_id='${user.id}')`) },
     },
+    {
+      idirUserid: session.idir_userid,
+    },
+  ];
+
+  const request = await models.request.findOne({
+    where,
   });
 
   const installation = await generateInstallation({
