67fb18a627d45d8e885d6b1f114b1f29ac298fd0
jsharman <jon.w.sharman@gmail.com>
2022-02-10 10:29:38 -0800

    feat: 772
    Implement request interface

--- app/page-partials/my-requests/ProjectTeamTabs.tsx
+++ app/page-partials/my-requests/ProjectTeamTabs.tsx
@@ -93,7 +93,7 @@ const NewEntityButton = ({
   tableTab?: string;
   handleNewTeamClick: Function;
   handleNewIntegrationClick: Function;
-  requests: any;
+  requests: Request[];
 }) => {
   if (tableTab === 'activeTeams')
     return (
