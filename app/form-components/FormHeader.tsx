2165fc65f72cb83c5bd28f60074a42c75e234624
Junmin Ahn <junminahn@outlook.com>
2022-02-14 11:20:05 -0800

    chore: update minor ui changes

--- app/form-components/FormHeader.tsx
+++ app/form-components/FormHeader.tsx
@@ -1,23 +1,26 @@
 import SHeader from 'components/SHeader';
-import { padStart } from 'lodash';
+import { padStart, isNil } from 'lodash';
 
 interface Props {
   formStage: number;
-  id: number | undefined;
+  requestId: number | undefined;
+  editing: boolean;
 }
 
-export default function Formheader({ formStage, id }: Props) {
-  const paddedId = padStart(String(id), 8, '0');
+export default function Formheader({ formStage, requestId, editing }: Props) {
+  const paddedId = padStart(String(requestId), 8, '0');
   const titles = [
-    `${id ? `Req ID: ${paddedId} - ` : ''}Enter requester information`,
+    `${isNil(requestId) ? '' : `Req ID: ${paddedId} - `}Enter requester information`,
     `Req ID: ${paddedId} - Choose providers and provide URIs`,
     `Req ID: ${paddedId} - Terms and Conditions`,
     `Req ID: ${paddedId} - Review and Submit`,
   ];
 
+  const prefix = editing ? 'Editing ' : '';
+
   return (
     <>
-      <SHeader>{titles[formStage]}</SHeader>
+      <SHeader>{prefix + titles[formStage]}</SHeader>
     </>
   );
 }
