1788321631b3d349e934b360008de49413e8eac2
Junmin Ahn <junminahn@outlook.com>
2022-05-19 16:26:34 -0700

    chore: block public configs

--- terraform/s3-bucket/main.tf
+++ terraform/s3-bucket/main.tf
@@ -32,8 +32,8 @@ resource "aws_s3_bucket_cors_configuration" "this" {
 resource "aws_s3_bucket_public_access_block" "this" {
   bucket = aws_s3_bucket.this.id
 
-  block_public_acls       = false
+  block_public_acls       = true
   block_public_policy     = true
-  ignore_public_acls      = false
+  ignore_public_acls      = true
   restrict_public_buckets = true
 }
