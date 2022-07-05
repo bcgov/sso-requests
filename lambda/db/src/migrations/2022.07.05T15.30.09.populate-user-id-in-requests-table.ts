eebfc6eb2512ef2d8ade61fff2b4294787914e00
Junmin Ahn <junminahn@outlook.com>
2022-07-05 15:55:24 -0700

    chore: ensure to fetch primary key to save integrations

--- lambda/db/src/migrations/2022.07.05T15.30.09.populate-user-id-in-requests-table.ts
+++ lambda/db/src/migrations/2022.07.05T15.30.09.populate-user-id-in-requests-table.ts
@@ -4,7 +4,7 @@ export const name = '2022.07.05T15.30.09.populate-user-id-in-requests-table';
 
 // see https://sequelize.org/master/manual/naming-strategies.html
 export const up = async ({ context: sequelize }) => {
-  const integrations = await models.request.findAll({ attributes: ['userId', 'idirUserid'] });
+  const integrations = await models.request.findAll({ attributes: ['id', 'userId', 'idirUserid'] });
   await Promise.all(
     integrations.map(async (integration) => {
       if (integration.userId) return;
