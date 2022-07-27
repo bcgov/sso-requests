958a328fd6e9e08a4292c18d61dc5e3a0ff47115
Junmin Ahn <junminahn@outlook.com>
2022-07-27 11:27:50 -0700

    chore: fix issue of displaying installation json for silver

--- app/jest/helpers.test.ts
+++ app/jest/helpers.test.ts
@@ -1,17 +1,5 @@
-import { idpToRealm, getRequestedEnvironments, canDeleteMember } from 'utils/helpers';
+import { getRequestedEnvironments, canDeleteMember } from 'utils/helpers';
 import { isValidKeycloakURI } from 'utils/validate';
-
-describe('idp to realm', () => {
-  it('gives the correct realm for different orders', () => {
-    expect(idpToRealm(['idir'])).toEqual('onestopauth');
-    expect(idpToRealm(['idir'])).toEqual('onestopauth');
-  });
-  it('works for all realms', () => {
-    expect(idpToRealm(['idir', 'bceid-basic'])).toEqual('onestopauth-basic');
-    expect(idpToRealm(['idir', 'bceid-business'])).toEqual('onestopauth-business');
-    expect(idpToRealm(['idir', 'bceid-business', 'bceid-basic'])).toEqual('onestopauth-both');
-  });
-});
 
 describe('kecloak URIs', () => {
   it('should validate the URI correctly', () => {
