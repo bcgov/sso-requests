c45be5ccc1e5e10397ef90b446b35fb70f341063
Junmin Ahn <junminahn@outlook.com>
2022-08-07 21:55:14 -0700

    chore: add backend logic to generate saml installation

---
+++ lambda/app/src/keycloak/installation/index.ts
@@ -0,0 +1,22 @@
+import { getAdminClient, getClient } from '../adminClient';
+import { updateClientSecret as oidcUpdateClientSecret, generateInstallation as oidcGenerateInstallation } from './oidc';
+import { generateInstallation as samlGenerateInstallation } from './saml';
+
+export const generateInstallation = async (data: {
+  serviceType: string;
+  environment: string;
+  realmName: string;
+  clientId: string;
+}) => {
+  const { serviceType, environment, realmName, clientId } = data;
+  const { kcAdminClient, authServerUrl } = await getAdminClient({ serviceType, environment });
+  const { realm, client } = await getClient(kcAdminClient, { serviceType, realmName, clientId });
+
+  if (client.protocol === 'openid-connect') {
+    return oidcGenerateInstallation({ kcAdminClient, realm, client, authServerUrl });
+  } else {
+    return samlGenerateInstallation({ kcAdminClient, realm, client, authServerUrl });
+  }
+};
+
+export const updateClientSecret = oidcUpdateClientSecret;
