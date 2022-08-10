d55c871459f6534f05c76c2385183a7c4a3e1397
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2022-08-10 10:41:38 -0700

    chore: initial css api project

---
+++ lambda/css_api/src/routes.ts
@@ -0,0 +1,50 @@
+import { wakeUpAll } from './controllers/heartbeat';
+
+const responseHeaders = {
+  'Content-Type': 'text/html; charset=utf-8',
+  'Access-Control-Allow-Headers': 'Content-Type,Authorization',
+  'Access-Control-Allow-Origin': 'https://bcgov.github.io',
+  'Access-Control-Allow-Methods': 'OPTIONS,PUT,GET',
+};
+
+const BASE_PATH = '/api';
+
+const handleError = (res, err) => {
+  console.error(err);
+  res.status(422).json({ success: false, message: err.message || err });
+};
+
+export const setRoutes = (app: any) => {
+  app.use((req, res, next) => {
+    res.set(responseHeaders);
+    if (next) next();
+  });
+
+  app.options('(.*)', async (req, res) => {
+    res.status(200).json(null);
+  });
+
+  app.get(`${BASE_PATH}/heartbeat`, async (req, res) => {
+    try {
+      const result = await wakeUpAll();
+      res.status(200).json(result);
+    } catch (err) {
+      handleError(res, err);
+    }
+  });
+
+  app.use((req, res, next) => {
+    try {
+      const { Authorization, authorization } = req.headers || {};
+      const authHeader = Authorization || authorization;
+      if (!authHeader || authHeader !== process.env.GH_SECRET) {
+        res.status(401).json({ success: false, message: 'not authorized' });
+        return false;
+      }
+    } catch (err) {
+      handleError(res, err);
+    }
+
+    if (next) next();
+  });
+};
