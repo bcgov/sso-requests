0cd0b8ea2a3e2a86c7bacf78fdff76037cb63ee8
Junmin Ahn <junminahn@outlook.com>
2022-08-18 15:33:37 -0700

    feat: add api certificate via tf

---
+++ terraform/certificates.tf
@@ -0,0 +1,20 @@
+resource "aws_acm_certificate" "this" {
+  domain_name       = var.custom_domain.name
+  validation_method = "DNS"
+}
+
+resource "aws_apigatewayv2_domain_name" "this" {
+  domain_name = var.custom_domain.name
+
+  domain_name_configuration {
+    certificate_arn = aws_acm_certificate.this.arn
+    endpoint_type   = "REGIONAL"
+    security_policy = "TLS_1_2"
+  }
+}
+
+resource "aws_apigatewayv2_api_mapping" "this" {
+  api_id      = aws_api_gateway_rest_api.sso_backend.id
+  domain_name = aws_apigatewayv2_domain_name.this.id
+  stage       = "test"
+}
