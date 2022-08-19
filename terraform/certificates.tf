4a1d585deed4dc0b392f708e0b2bc37f0077ff48
Junmin Ahn <junminahn@outlook.com>
2022-08-19 07:49:31 -0700

    chore: comment agw domain name

--- terraform/certificates.tf
+++ terraform/certificates.tf
@@ -3,18 +3,18 @@ resource "aws_acm_certificate" "this" {
   validation_method = "DNS"
 }
 
-resource "aws_apigatewayv2_domain_name" "this" {
-  domain_name = var.custom_domain_name
+# resource "aws_apigatewayv2_domain_name" "this" {
+#   domain_name = var.custom_domain_name
 
-  domain_name_configuration {
-    certificate_arn = aws_acm_certificate.this.arn
-    endpoint_type   = "REGIONAL"
-    security_policy = "TLS_1_2"
-  }
-}
+#   domain_name_configuration {
+#     certificate_arn = aws_acm_certificate.this.arn
+#     endpoint_type   = "REGIONAL"
+#     security_policy = "TLS_1_2"
+#   }
+# }
 
-resource "aws_apigatewayv2_api_mapping" "this" {
-  api_id      = aws_api_gateway_rest_api.sso_backend.id
-  domain_name = aws_apigatewayv2_domain_name.this.id
-  stage       = "test"
-}
+# resource "aws_apigatewayv2_api_mapping" "this" {
+#   api_id      = aws_api_gateway_rest_api.sso_backend.id
+#   domain_name = aws_apigatewayv2_domain_name.this.id
+#   stage       = "test"
+# }
