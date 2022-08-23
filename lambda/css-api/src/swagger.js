efd23972011c7a967b2b4fa60619aa3503e03fd5
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2022-08-23 08:29:18 -0700

    chore: updated host and makefile

--- lambda/css-api/src/swagger.js
+++ lambda/css-api/src/swagger.js
@@ -6,8 +6,12 @@ const doc = {
     title: 'CSS SSO API',
     description: 'CSS SSO API Service by BC Gov SSO Team',
   },
-  host: 'localhost:3000',
-  basePath: '/api/v1',
+  host: `${
+    process.env.APP_ENV === 'production'
+      ? 'kgodz1zmk2.execute-api.ca-central-1.amazonaws.com'
+      : 'api-dev.loginproxy.gov.bc.ca'
+  }`,
+  basePath: '/test/api/v1',
   schemes: ['https'],
   tags: [
     {
