7f0cd342aad5677c0a9840c225dde14d7cbd34ee
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2022-08-24 13:55:58 -0700

    chore: adding mime types for uploding files

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,7 +1,12 @@
+locals {
+  mime_types = jsondecode(file("${path.module}/mime-types.json"))
+}
+
 resource "aws_s3_object" "swagger_dist" {
-  for_each = fileset("swagger-ui-dist/", "*")
-  bucket   = module.s3_sso_api_swagger.bucket_id
-  key      = each.value
-  source   = "swagger-ui-dist/${each.value}"
-  etag     = filemd5("swagger-ui-dist/${each.value}")
+  for_each     = fileset("swagger-ui-dist/", "*")
+  bucket       = module.s3_sso_api_swagger.bucket_id
+  key          = each.value
+  source       = "swagger-ui-dist/${each.value}"
+  etag         = filemd5("swagger-ui-dist/${each.value}")
+  content_type = lookup(local.mime_types, regex("\\.[^.]+$", each.value), null)
 }
