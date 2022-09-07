0dd9ffb7f8eb6f90a797946430fc0bccff5dc070
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2022-09-07 11:45:00 -0700

    chore: added more unit tests

--- lambda/__tests__/31.css-api.test.ts
+++ lambda/__tests__/31.css-api.test.ts
@@ -7,6 +7,28 @@ import { Integration } from './helpers/integration';
 
 const BASE_PATH = '/api/v1';
 const TEST_TOKEN = 'testtoken';
+const integrationRoles = ['role1', 'role2', 'role3'];
+const integrationRoleUsers = [
+  {
+    id: 'f1b2b019-b657-4854-961b-739469d16c88',
+    createdTimestamp: 1661664154156,
+    username: '08fe82212408466081ea023cf0ec945d@idir',
+    enabled: true,
+    email: 'BRTEST@gmail.com',
+    totp: false,
+    emailVerified: false,
+    firstName: 'BRTEST',
+    lastName: '2',
+    attributes: {
+      idir_userid: ['08FE82212408466081EA023CF0EC945D'],
+      displayName: ['2, BRTEST'],
+    },
+    disableableCredentialTypes: [],
+    requiredActions: [],
+    notBefore: 0,
+  },
+];
+const integrationUserRoles = ['role1'];
 let integration = new Integration();
 
 jest.mock('@lambda-app/authenticate');
@@ -14,6 +36,26 @@ jest.mock('@lambda-app/github', () => {
   return {
     dispatchRequestWorkflow: jest.fn(() => ({ status: 204 })),
     closeOpenPullRequests: jest.fn(() => Promise.resolve()),
+  };
+});
+jest.mock('@lambda-app/keycloak/users', () => {
+  return {
+    listClientRoles: jest.fn(() => {
+      return Promise.resolve(integrationRoles);
+    }),
+    createRole: jest.fn(() => Promise.resolve()),
+    updateRole: jest.fn(() => Promise.resolve()),
+    deleteRole: jest.fn(() => Promise.resolve()),
+    listRoleUsers: jest.fn(() => {
+      return Promise.resolve(integrationRoleUsers);
+    }),
+    listUserRoles: jest.fn(() => {
+      return Promise.resolve(integrationUserRoles);
+    }),
+    findUserByRealm: jest.fn(() => {
+      return Promise.resolve(integrationRoleUsers);
+    }),
+    manageUserRole: jest.fn(() => Promise.resolve()),
   };
 });
 jest.mock('@lambda-app/helpers/token', () => {
@@ -77,15 +119,128 @@ describe('create team and gold integration', () => {
     jest.clearAllMocks();
   });
 
+  afterAll(async () => {
+    const res = await integration.delete();
+    console.log(res);
+  });
+
   it('gets api heartbeat', async () => {
     await supertest(app).get(`${BASE_PATH}/heartbeat`).expect(200);
   });
+
   it('gets team integrations', async () => {
     const result = await supertest(app).get(`${BASE_PATH}/integrations`).expect(200);
+    expect(result.body.success).toBe(true);
     expect(result.body.data.length).toBe(1);
   });
+
   it('gets team integration by id', async () => {
     const result = await supertest(app).get(`${BASE_PATH}/integrations/${integration.int.id}`).expect(200);
+    expect(result.body.success).toBe(true);
     expect(result.body.data.id).toBe(integration.int.id);
+  });
+
+  it('gets team integration roles by environment', async () => {
+    const result = await supertest(app).get(`${BASE_PATH}/integrations/${integration.int.id}/dev/roles`).expect(200);
+    expect(result.body.success).toBe(true);
+    result.body.data.forEach((role) => {
+      expect(integrationRoles).toContain(role.name);
+    });
+  });
+
+  it('gets team integration role by environment and role name', async () => {
+    const result = await supertest(app)
+      .get(`${BASE_PATH}/integrations/${integration.int.id}/dev/roles/${integrationRoles[0]}`)
+      .expect(200);
+    expect(result.body.success).toBe(true);
+    expect(integrationRoles[0]).toBe(result.body.data.name);
+  });
+
+  it('creates team integration role for an environment', async () => {
+    const result = await supertest(app)
+      .post(`${BASE_PATH}/integrations/${integration.int.id}/dev/roles`)
+      .send({ name: 'role4' })
+      .set('Accept', 'application/json')
+      .expect(201);
+    expect(result.body.success).toBe(true);
+    expect(result.body.message).toBe('created');
+  });
+
+  it('updates team integration role for an environment', async () => {
+    const result = await supertest(app)
+      .put(`${BASE_PATH}/integrations/${integration.int.id}/dev/roles/role4`)
+      .send({ name: 'role5' })
+      .set('Accept', 'application/json')
+      .expect(200);
+    expect(result.body.success).toBe(true);
+    expect(result.body.message).toBe('updated');
+  });
+
+  it('deletes team integration role for an environment', async () => {
+    const result = await supertest(app)
+      .delete(`${BASE_PATH}/integrations/${integration.int.id}/dev/roles/role4`)
+      .expect(200);
+    expect(result.body.success).toBe(true);
+    expect(result.body.message).toBe('deleted');
+  });
+
+  it('fails fetching team integration role mappings for an environment when role name and username is not supplied', async () => {
+    const result = await supertest(app)
+      .get(`${BASE_PATH}/integrations/${integration.int.id}/dev/user-role-mappings`)
+      .expect(422);
+    expect(result.body.success).toBe(false);
+  });
+
+  it('gets team integration role mappings for an environment when role name is supplied', async () => {
+    const result = await supertest(app)
+      .get(`${BASE_PATH}/integrations/${integration.int.id}/dev/user-role-mappings`)
+      .query({ roleName: integrationUserRoles[0] })
+      .expect(200);
+
+    expect(result.body.success).toBe(true);
+    expect(result.body.data.roles.length === 1).toBe(true);
+    expect(result.body.data.roles[0].name).toBe(integrationUserRoles[0]);
+    expect(result.body.data.users.length === 1).toBe(true);
+    expect(result.body.data.users[0].username).toBe(integrationRoleUsers[0].username);
+    expect(result.body.data.users[0].firstName).toBe(integrationRoleUsers[0].firstName);
+    expect(result.body.data.users[0].lastName).toBe(integrationRoleUsers[0].lastName);
+    expect(result.body.data.users[0].email).toBe(integrationRoleUsers[0].email);
+    expect(result.body.data.users[0].attributes).toEqual(integrationRoleUsers[0].attributes);
+  });
+
+  it('gets team integration role mappings for an environment when username is supplied', async () => {
+    const result = await supertest(app)
+      .get(`${BASE_PATH}/integrations/${integration.int.id}/dev/user-role-mappings`)
+      .query({ username: integrationRoleUsers[0].username })
+      .expect(200);
+
+    expect(result.body.success).toBe(true);
+    expect(result.body.data.roles.length > 0).toBe(true);
+    expect(result.body.data.roles[0].name).toBe(integrationUserRoles[0]);
+    expect(result.body.data.users.length === 1).toBe(true);
+    expect(result.body.data.users[0].username).toBe(integrationRoleUsers[0].username);
+    expect(result.body.data.users[0].firstName).toBe(integrationRoleUsers[0].firstName);
+    expect(result.body.data.users[0].lastName).toBe(integrationRoleUsers[0].lastName);
+    expect(result.body.data.users[0].email).toBe(integrationRoleUsers[0].email);
+    expect(result.body.data.users[0].attributes).toEqual(integrationRoleUsers[0].attributes);
+  });
+
+  it('creates team integration role mapping for an environment', async () => {
+    const result = await supertest(app)
+      .post(`${BASE_PATH}/integrations/${integration.int.id}/dev/user-role-mappings`)
+      .send({ username: integrationRoleUsers[0].username, roleName: 'role5', operation: 'add' })
+      .set('Accept', 'application/json')
+      .expect(201);
+    expect(result.body.success).toBe(true);
+    expect(result.body.message).toBe('created');
+  });
+  it('deletes team integration role mapping for an environment', async () => {
+    const result = await supertest(app)
+      .post(`${BASE_PATH}/integrations/${integration.int.id}/dev/user-role-mappings`)
+      .send({ username: integrationRoleUsers[0].username, roleName: 'role5', operation: 'del' })
+      .set('Accept', 'application/json')
+      .expect(200);
+    expect(result.body.success).toBe(true);
+    expect(result.body.message).toBe('deleted');
   });
 });
--- lambda/__tests__/helpers/server.ts
+++ lambda/__tests__/helpers/server.ts
@@ -1,8 +1,10 @@
 import 'reflect-metadata';
 import express from 'express';
 import { setRoutes } from '../../css-api/src/routes';
+import bodyParser from 'body-parser';
 
 const app = express();
+app.use(bodyParser.json());
 const router = express.Router();
 setRoutes(router);
 
