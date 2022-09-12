b8a75a07398632ce2b2c6ba5a1b56d28b9376b8c
Junmin Ahn <junminahn@outlook.com>
2022-09-12 12:17:45 -0700

    chore: encapsulate common parts for bceid & github tab contents

--- app/page-partials/admin-dashboard/AdminTabs/BceidTabContent.tsx
+++ app/page-partials/admin-dashboard/AdminTabs/BceidTabContent.tsx
@@ -1,17 +1,5 @@
-import styled from 'styled-components';
-import Button from '@button-inc/bcgov-theme/Button';
-import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
-import CenteredModal from 'components/CenteredModal';
-import { updateRequest } from 'services/request';
 import { Integration } from 'interfaces/Request';
-import SubmittedStatusIndicator from 'components/SubmittedStatusIndicator';
-
-const TabWrapper = styled.div`
-  padding-left: 1rem;
-  padding-right: 1rem;
-`;
-
-const modalId = 'bceid-approval-modal';
+import TabContent from './TabContent';
 
 interface Props {
   integration?: Integration;
@@ -22,58 +10,17 @@ function BceidTabContent({ integration, onApproved }: Props) {
   if (!integration) return null;
   const { status, bceidApproved } = integration;
 
-  const canApproveProd = status === 'applied' && !bceidApproved;
-  const awaitingProdComplete = status !== 'applied' && bceidApproved;
-
-  const openModal = () => (window.location.hash = modalId);
-
-  let content;
-  if (canApproveProd) {
-    content = (
-      <>
-        <p>To begin the BCeID integration in production, Click Below.</p>
-        <Button onClick={openModal}>Approve Prod</Button>
-      </>
-    );
-  } else if (awaitingProdComplete) {
-    content = (
-      <SubmittedStatusIndicator
-        integration={integration}
-        title="The terraform script will take approximately 20 minutes to complete."
-      />
-    );
-  } else {
-    content = <p>This integration has already been approved.</p>;
-  }
-
-  const onConfirm = async () => {
-    const [, err] = await updateRequest(
-      {
-        id: integration.id,
-        bceidApproved: true,
-      },
-      true,
-    );
-
-    if (onApproved && !err) {
-      onApproved();
-    }
-  };
+  const canApproveProd = (status === 'applied' && !bceidApproved) || false;
+  const awaitingProdComplete = (status !== 'applied' && bceidApproved) || false;
 
   return (
-    <>
-      <TabWrapper>
-        <br />
-        {content}
-      </TabWrapper>
-      <CenteredModal
-        id={modalId}
-        content="Are you sure you want to approve this integration for BCeID production?"
-        onConfirm={onConfirm}
-        icon={faExclamationTriangle}
-        title="BCeID Approve"
-      />
-    </>
+    <TabContent
+      type="bceid"
+      integration={integration}
+      canApproveProd={canApproveProd}
+      awaitingProdComplete={awaitingProdComplete}
+      onApproved={onApproved}
+    />
   );
 }
 
--- app/page-partials/admin-dashboard/AdminTabs/GithubTabContent.tsx
+++ app/page-partials/admin-dashboard/AdminTabs/GithubTabContent.tsx
@@ -1,17 +1,5 @@
-import styled from 'styled-components';
-import Button from '@button-inc/bcgov-theme/Button';
-import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
-import CenteredModal from 'components/CenteredModal';
-import { updateRequest } from 'services/request';
 import { Integration } from 'interfaces/Request';
-import SubmittedStatusIndicator from 'components/SubmittedStatusIndicator';
-
-const TabWrapper = styled.div`
-  padding-left: 1rem;
-  padding-right: 1rem;
-`;
-
-const modalId = 'github-approval-modal';
+import TabContent from './TabContent';
 
 interface Props {
   integration?: Integration;
@@ -22,58 +10,17 @@ function GithubTabContent({ integration, onApproved }: Props) {
   if (!integration) return null;
   const { status, githubApproved } = integration;
 
-  const canApproveProd = status === 'applied' && !githubApproved;
-  const awaitingProdComplete = status !== 'applied' && githubApproved;
-
-  const openModal = () => (window.location.hash = modalId);
-
-  let content;
-  if (canApproveProd) {
-    content = (
-      <>
-        <p>To begin the GitHub integration in production, Click Below.</p>
-        <Button onClick={openModal}>Approve Prod</Button>
-      </>
-    );
-  } else if (awaitingProdComplete) {
-    content = (
-      <SubmittedStatusIndicator
-        integration={integration}
-        title="The terraform script will take approximately 20 minutes to complete."
-      />
-    );
-  } else {
-    content = <p>This integration has already been approved.</p>;
-  }
-
-  const onConfirm = async () => {
-    const [, err] = await updateRequest(
-      {
-        id: integration.id,
-        githubApproved: true,
-      },
-      true,
-    );
-
-    if (onApproved && !err) {
-      onApproved();
-    }
-  };
+  const canApproveProd = (status === 'applied' && !githubApproved) || false;
+  const awaitingProdComplete = (status !== 'applied' && githubApproved) || false;
 
   return (
-    <>
-      <TabWrapper>
-        <br />
-        {content}
-      </TabWrapper>
-      <CenteredModal
-        id={modalId}
-        content="Are you sure you want to approve this integration for GitHub production?"
-        onConfirm={onConfirm}
-        icon={faExclamationTriangle}
-        title="GitHub Approve"
-      />
-    </>
+    <TabContent
+      type="github"
+      integration={integration}
+      canApproveProd={canApproveProd}
+      awaitingProdComplete={awaitingProdComplete}
+      onApproved={onApproved}
+    />
   );
 }
 
