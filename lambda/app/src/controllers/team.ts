a4cbac984fc43054df16599a70b39040e78cd669
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2022-09-25 09:36:36 -0700

    chore: added validation to restrict teams to only one SA

--- lambda/app/src/controllers/team.ts
+++ lambda/app/src/controllers/team.ts
@@ -185,6 +185,12 @@ export const updateMemberInTeam = async (teamId: number, userId: number, data: {
 };
 
 export const requestServiceAccount = async (session: Session, userId: number, teamId: number, requester: string) => {
+  const existingServiceAccounts = await getServiceAccounts(userId, teamId);
+
+  if (existingServiceAccounts.length > 0) {
+    throw Error('CSS API Account already generated for this team');
+  }
+
   const teamIdLiteral = getTeamIdLiteralOutOfRange(userId, teamId, ['admin']);
   const integrations = await getIntegrationsByTeam(teamId, 'gold');
   const team = await getTeamById(teamId);
@@ -206,12 +212,12 @@ export const requestServiceAccount = async (session: Session, userId: number, te
   serviceAccount.clientId = `service-account-team-${teamId}-${serviceAccount.id}`;
   serviceAccount.requester = requester;
 
-  const ghResult = await dispatchRequestWorkflow(serviceAccount);
+  // const ghResult = await dispatchRequestWorkflow(serviceAccount);
 
-  if (ghResult.status !== 204) {
-    await serviceAccount.destroy();
-    throw Error('failed to create a workflow dispatch event');
-  }
+  // if (ghResult.status !== 204) {
+  //   await serviceAccount.destroy();
+  //   throw Error('failed to create a workflow dispatch event');
+  // }
 
   await serviceAccount.save();
 
@@ -292,16 +298,16 @@ export const deleteServiceAccount = async (session: Session, userId: number, tea
     serviceAccount.status = 'submitted';
     serviceAccount.archived = true;
 
-    if (isMerged) {
-      // Trigger workflow with empty environments to delete client
-      const ghResult = await dispatchRequestWorkflow(serviceAccount);
-      if (ghResult.status !== 204) {
-        throw Error('failed to create a workflow dispatch event');
-      }
-    }
+    // if (isMerged) {
+    //   // Trigger workflow with empty environments to delete client
+    //   const ghResult = await dispatchRequestWorkflow(serviceAccount);
+    //   if (ghResult.status !== 204) {
+    //     throw Error('failed to create a workflow dispatch event');
+    //   }
+    // }
 
-    // Close any pr's if they exist
-    await closeOpenPullRequests(saId);
+    // // Close any pr's if they exist
+    // await closeOpenPullRequests(saId);
 
     await serviceAccount.save();
 
