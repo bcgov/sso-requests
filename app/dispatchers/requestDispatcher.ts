5d96f52405a18f10bc8119ffdc6d348e16da341b
Junmin Ahn <junminahn@outlook.com>
2022-10-05 12:19:26 -0700

    chore: remove reduce state from integration tabs

--- app/dispatchers/requestDispatcher.ts
+++ app/dispatchers/requestDispatcher.ts
@@ -3,7 +3,3 @@ import type { Action } from 'reducers/dashboardReducer';
 export const $setDownloadError = (payload: boolean | null): Action => {
   return { type: 'setDownloadError', payload };
 };
-
-export const $setPanelTab = (payload?: string): Action => {
-  return { type: 'setPanelTab', payload };
-};
--- app/reducers/dashboardReducer.ts
+++ app/reducers/dashboardReducer.ts
@@ -1,9 +1,8 @@
 export interface DashboardReducerState {
-  panelTab?: string;
   downloadError?: boolean;
 }
 
-export type ActionTypes = 'setPanelTab' | 'setDownloadError';
+export type ActionTypes = 'setDownloadError';
 
 export interface Action {
   type: ActionTypes;
@@ -14,9 +13,6 @@ export const initialState: DashboardReducerState = {};
 
 const reducer = (state: DashboardReducerState, action: Action) => {
   switch (action.type) {
-    case 'setPanelTab': {
-      return { ...state, panelTab: action.payload };
-    }
     case 'setDownloadError': {
       return { ...state, downloadError: action.payload };
     }
