91ecb8a13498c56c4bbb295bd31d68d9d5a5db13
Junmin Ahn <junminahn@outlook.com>
2022-10-15 10:38:02 -0700

    chore: enable logging for debuging

--- lambda/app/src/routes.ts
+++ lambda/app/src/routes.ts
@@ -79,6 +79,7 @@ const handleError = (res, err) => {
   if (isString(message)) {
     message = tryJSON(message);
   }
+  console.log({ success: false, message });
   res.status(422).json({ success: false, message });
 };
 
