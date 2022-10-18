41bd4009a9b7378f230edc338c3953daac769b0a
Junmin Ahn <junminahn@outlook.com>
2022-10-18 16:17:46 -0700

    chore: prevent rds auto pause for 8 hours

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -28,7 +28,7 @@ module "db" {
     auto_pause               = true
     min_capacity             = 2
     max_capacity             = 4
-    seconds_until_auto_pause = 3600
+    seconds_until_auto_pause = 28800
     timeout_action           = "ForceApplyCapacityChange"
   }
 
