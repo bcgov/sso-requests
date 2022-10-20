b4a931a7056873e3ef3824f30253d2fc593cfd21
Junmin Ahn <junminahn@outlook.com>
2022-10-20 11:07:57 -0700

    chore: revert changes in app lambda entrypoint

--- lambda/app/src/main.ts
+++ lambda/app/src/main.ts
@@ -1,16 +1,16 @@
 import { APIGatewayProxyEvent, Context, Callback } from 'aws-lambda';
-import serverless from 'serverless-http';
-import express from 'express';
+const Api = require('lambda-api-router');
 import { setRoutes } from './routes';
+import { sequelize } from '@lambda-shared/sequelize/models/models';
+import { expressyApiRouter } from '@lambda-shared/helpers/expressy-api-router';
 
-const app = express();
+const app = new Api();
+expressyApiRouter(app);
 setRoutes(app);
 
-const apiHandler = serverless(app);
 export const handler = async (event: APIGatewayProxyEvent, context?: Context) => {
   console.log('Event: ', event);
   if (context) context.callbackWaitsForEmptyEventLoop = false;
 
-  const result = await apiHandler(event, context);
-  return result;
+  return app.listen(event, context);
 };
