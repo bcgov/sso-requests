7b8193dddf07eb0a659d6703a63196c3e143f047
Junmin Ahn <junminahn@outlook.com>
2022-10-21 15:10:55 -0700

    chore: remove api gateway cors config

--- terraform/api-gateway-app.tf
+++ terraform/api-gateway-app.tf
@@ -28,34 +28,34 @@ resource "aws_api_gateway_integration" "app" {
   uri                     = aws_lambda_function.app.invoke_arn
 }
 
-resource "aws_api_gateway_method_response" "app_cors" {
-  rest_api_id = aws_api_gateway_rest_api.sso_backend.id
-  resource_id = aws_api_gateway_method.app_proxy.resource_id
-  http_method = aws_api_gateway_method.app_proxy.http_method
-  status_code = 200
+# resource "aws_api_gateway_method_response" "app_cors" {
+#   rest_api_id = aws_api_gateway_rest_api.sso_backend.id
+#   resource_id = aws_api_gateway_method.app_proxy.resource_id
+#   http_method = aws_api_gateway_method.app_proxy.http_method
+#   status_code = 200
 
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin"      = true,
-    "method.response.header.Access-Control-Allow-Headers"     = true,
-    "method.response.header.Access-Control-Allow-Methods"     = true,
-    "method.response.header.Access-Control-Allow-Credentials" = true,
-  }
+#   response_parameters = {
+#     "method.response.header.Access-Control-Allow-Origin"      = true,
+#     "method.response.header.Access-Control-Allow-Headers"     = true,
+#     "method.response.header.Access-Control-Allow-Methods"     = true,
+#     "method.response.header.Access-Control-Allow-Credentials" = true,
+#   }
 
-  depends_on = [aws_api_gateway_method.app_proxy]
-}
+#   depends_on = [aws_api_gateway_method.app_proxy]
+# }
 
-resource "aws_api_gateway_integration_response" "app_cors" {
-  rest_api_id = aws_api_gateway_rest_api.sso_backend.id
-  resource_id = aws_api_gateway_method.app_proxy.resource_id
-  http_method = aws_api_gateway_method.app_proxy.http_method
-  status_code = 200
+# resource "aws_api_gateway_integration_response" "app_cors" {
+#   rest_api_id = aws_api_gateway_rest_api.sso_backend.id
+#   resource_id = aws_api_gateway_method.app_proxy.resource_id
+#   http_method = aws_api_gateway_method.app_proxy.http_method
+#   status_code = 200
 
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin"      = "'https://bcgov.github.io'",
-    "method.response.header.Access-Control-Allow-Headers"     = "'Content-Type,Authorization'",
-    "method.response.header.Access-Control-Allow-Methods"     = "'OPTIONS,POST,GET,PUT,DELETE'"
-    "method.response.header.Access-Control-Allow-Credentials" = "'true'"
-  }
+#   response_parameters = {
+#     "method.response.header.Access-Control-Allow-Origin"      = "'https://bcgov.github.io'",
+#     "method.response.header.Access-Control-Allow-Headers"     = "'Content-Type,Authorization'",
+#     "method.response.header.Access-Control-Allow-Methods"     = "'OPTIONS,POST,GET,PUT,DELETE'"
+#     "method.response.header.Access-Control-Allow-Credentials" = "'true'"
+#   }
 
-  depends_on = [aws_api_gateway_integration.app, aws_api_gateway_method_response.app_cors]
-}
+#   depends_on = [aws_api_gateway_integration.app, aws_api_gateway_method_response.app_cors]
+# }
