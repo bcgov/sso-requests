0bcf337ccf53a8ee6b9eb391a0f4684cf53bd824
Junmin Ahn <junminahn@outlook.com>
2022-10-21 11:04:46 -0700

    chore: revert changes around serverless routes

--- lambda/app/src/main.ts
+++ lambda/app/src/main.ts
@@ -1,29 +1,16 @@
 import { APIGatewayProxyEvent, Context, Callback } from 'aws-lambda';
-import serverless from 'serverless-http';
-import express from 'express';
+const Api = require('lambda-api-router');
 import { setRoutes } from './routes';
+import { sequelize } from '@lambda-shared/sequelize/models/models';
+import { expressyApiRouter } from '@lambda-shared/helpers/expressy-api-router';
 
-const tryJSON = (str) => {
-  try {
-    return JSON.parse(str);
-  } catch {
-    return str;
-  }
-};
-
-const app = express();
-app.use((req, res, next) => {
-  req.body = req.body.toString();
-  req.body = tryJSON(req.body);
-  next();
-});
+const app = new Api();
+expressyApiRouter(app);
 setRoutes(app);
 
-const apiHandler = serverless(app);
 export const handler = async (event: APIGatewayProxyEvent, context?: Context) => {
   console.log('Event: ', event);
   if (context) context.callbackWaitsForEmptyEventLoop = false;
 
-  const result = await apiHandler(event, context);
-  return result as any;
+  return app.listen(event, context);
 };
