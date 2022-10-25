702b039c3338f5a10d92dde8921b6d931ca0d9ea
Junmin Ahn <junminahn@outlook.com>
2022-10-24 20:59:08 -0700

    chore: refactor express route

--- lambda/app/src/main.ts
+++ lambda/app/src/main.ts
@@ -15,6 +15,7 @@ const tryJSON = (str) => {
 const app = express();
 const router = express.Router();
 app.set('etag', false);
+app.disable('x-powered-by');
 
 router.use((req, res, next) => {
   req.body = req.body.toString();
@@ -22,11 +23,9 @@ router.use((req, res, next) => {
   next();
 });
 
-const allowedOrigin = process.env.LOCAL_DEV === 'true' ? '*' : 'https://bcgov.github.io';
-
 router.use(
   cors({
-    origin: allowedOrigin,
+    origin: process.env.LOCAL_DEV === 'true' ? '*' : 'https://bcgov.github.io',
     methods: ['OPTIONS', 'GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
@@ -44,7 +43,6 @@ export const handler = async (event: APIGatewayProxyEvent, context?: Context) =>
 
   const result: any = await apiHandler(event, context);
 
-  delete result.headers['x-powered-by'];
   console.log('Result: ', result);
   return result as any;
 };
