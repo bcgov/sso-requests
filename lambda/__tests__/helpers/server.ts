6188255af9963be5145e90602f1e92f0b904ef4f
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2022-10-27 13:37:30 -0700

    chore: fixed unit tests for css api

--- lambda/__tests__/helpers/server.ts
+++ lambda/__tests__/helpers/server.ts
@@ -2,13 +2,36 @@ import 'reflect-metadata';
 import express from 'express';
 import { setRoutes } from '../../css-api/src/routes';
 import bodyParser from 'body-parser';
+import cors from 'cors';
+
+const tryJSON = (str) => {
+  try {
+    return JSON.parse(str);
+  } catch {
+    return str;
+  }
+};
 
 const app = express();
 app.use(bodyParser.json());
 const router = express.Router();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set('etag', false);
+app.disable('x-powered-by');
+
+router.use(
+  cors({
+    origin: '*',
+    methods: ['OPTIONS', 'GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  }),
+);
+
 setRoutes(router);
 
-app.use('/', router);
+app.use('/api/v1', router);
 
 app.use(express.json());
 
--- lambda/css-api/src/main.ts
+++ lambda/css-api/src/main.ts
@@ -4,20 +4,6 @@ import serverless from 'serverless-http';
 import express from 'express';
 import cors from 'cors';
 import { setRoutes } from './routes';
-import { expressyApiRouter } from '@lambda-shared/helpers/expressy-api-router';
-
-// const app = new Api();
-
-// expressyApiRouter(app);
-// setRoutes(app);
-
-// export const handler = async (event: APIGatewayProxyEvent, context?: Context) => {
-//   // Sequelize waits ~10seconds to drop connection, delaying API response.
-//   // Use this to prevent, see https://forum.serverless.com/t/lambda-with-rds-using-vpc-works-slow/1261/7 for more
-//   if (context) context.callbackWaitsForEmptyEventLoop = false;
-
-//   return app.listen(event, context);
-// };
 
 const tryJSON = (str) => {
   try {
