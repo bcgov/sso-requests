a13bb217dbe158e7c5b0b21c6159b3093e1a77c2
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2022-10-27 09:58:56 -0700

    chore: debugging the logs

--- lambda/css-api/k6-tests/cleanup.sh
+++ lambda/css-api/k6-tests/cleanup.sh
@@ -4,13 +4,10 @@ ACCESS_TOKEN=$(curl -X POST -d grant_type=client_credentials -d client_id=$K6_CL
 
 INTEGRATION_ID=$(curl -X GET $K6_CSS_API_URL/integrations -H "Authorization: Bearer $ACCESS_TOKEN" | jq '.data | .[].id' -r)
 
-if [ -z "$INTEGRATION_ID" ];
-then
-    LIST_OF_ROLES=$(curl -X GET $K6_CSS_API_URL/integrations/$INTEGRATION_ID/$K6_ENVIRONMENT/roles -H "Authorization: Bearer $ACCESS_TOKEN" | jq '.[] | .[].name' -r)
+LIST_OF_ROLES=$(curl -X GET $K6_CSS_API_URL/integrations/$INTEGRATION_ID/$K6_ENVIRONMENT/roles -H "Authorization: Bearer $ACCESS_TOKEN" | jq '.[] | .[].name' -r)
 
-    for i in $(echo "$LIST_OF_ROLES" | tr ',' '\n')
-    do
-        echo "$K6_CSS_API_URL/integrations/$INTEGRATION_ID/$K6_ENVIRONMENT/roles/$i"
-        curl -X DELETE "$K6_CSS_API_URL/integrations/$INTEGRATION_ID/$K6_ENVIRONMENT/roles/$i" -H "Authorization: Bearer $ACCESS_TOKEN"
-    done
-fi;
+for i in $(echo "$LIST_OF_ROLES" | tr ',' '\n')
+do
+    echo "$K6_CSS_API_URL/integrations/$INTEGRATION_ID/$K6_ENVIRONMENT/roles/$i"
+    curl -X DELETE "$K6_CSS_API_URL/integrations/$INTEGRATION_ID/$K6_ENVIRONMENT/roles/$i" -H "Authorization: Bearer $ACCESS_TOKEN"
+done
