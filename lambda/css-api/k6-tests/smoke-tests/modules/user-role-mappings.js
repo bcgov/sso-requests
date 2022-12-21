e7c3b55db27b19cbefc7316a7ceb7a93aacc8c52
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2022-12-21 13:36:51 -0800

    chore: added new endpoints to manage user role mapping

--- lambda/css-api/k6-tests/smoke-tests/modules/user-role-mappings.js
+++ lambda/css-api/k6-tests/smoke-tests/modules/user-role-mappings.js
@@ -341,6 +341,287 @@ export function testUserRoleMapping(options) {
     }
   });
 
+  group('POST roles to user', () => {
+    {
+      const url =
+        __ENV.css_api_url +
+        `/integrations/${integrationId}/${__ENV.environment}/users/${__ENV.username}/roles?param1=1&param2=2`;
+      const requestOptions = Object.assign({}, options);
+      requestOptions.headers.Accept = 'application/json';
+      const body = [{ name: 'role-mapping' }];
+      const response = http.post(url, JSON.stringify(body), requestOptions);
+
+      console.debug(`Response from CSS API: ${JSON.stringify(response, 0, 2)}`);
+
+      check(response, {
+        'should return 400 when arbitrary query params passed': (r) => r.status === 400,
+      });
+
+      sleep(SLEEP_DURATION);
+    }
+
+    {
+      const url =
+        __ENV.css_api_url + `/integrations/${integrationId}/${__ENV.environment}/users/${__ENV.username}/roles`;
+      const requestOptions = Object.assign({}, options);
+      requestOptions.headers.Accept = 'application/json';
+      const body = {};
+      const response = http.post(url, JSON.stringify(body), requestOptions);
+
+      console.debug(`Response from CSS API: ${JSON.stringify(response, 0, 2)}`);
+
+      check(response, {
+        'should return 400 when invalid payload passed': (r) => r.status === 400,
+      });
+
+      sleep(SLEEP_DURATION);
+    }
+    {
+      const url =
+        __ENV.css_api_url + `/integrations/${integrationId}/${__ENV.environment}/users/${__ENV.username}/roles`;
+      const requestOptions = Object.assign({}, options);
+      requestOptions.headers.Accept = 'application/json';
+      const body = [{ name: 'nonexistentrole' }];
+      const response = http.post(url, JSON.stringify(body), requestOptions);
+
+      console.debug(`Response from CSS API: ${JSON.stringify(response, 0, 2)}`);
+
+      check(response, {
+        'should return 404 when non-existent role name passed': (r) => r.status === 404,
+      });
+
+      sleep(SLEEP_DURATION);
+    }
+    {
+      const url = __ENV.css_api_url + `/integrations/${integrationId}/${__ENV.environment}/users/nonexistentuser/roles`;
+      const requestOptions = Object.assign({}, options);
+      requestOptions.headers.Accept = 'application/json';
+      const body = [{ name: 'role-mapping' }];
+      const response = http.post(url, JSON.stringify(body), requestOptions);
+
+      console.debug(`Response from CSS API: ${JSON.stringify(response, 0, 2)}`);
+
+      check(response, {
+        'should return 400 when user with invalid or no idp passed': (r) => r.status === 400,
+      });
+
+      sleep(SLEEP_DURATION);
+    }
+    {
+      const url =
+        __ENV.css_api_url + `/integrations/${integrationId}/${__ENV.environment}/users/nonexistentuser@idir/roles`;
+      const requestOptions = Object.assign({}, options);
+      requestOptions.headers.Accept = 'application/json';
+      const body = [{ name: 'role-mapping' }];
+      const response = http.post(url, JSON.stringify(body), requestOptions);
+
+      console.debug(`Response from CSS API: ${JSON.stringify(response, 0, 2)}`);
+
+      check(response, {
+        'should return 404 when non-existent username passed': (r) => r.status === 404,
+      });
+
+      sleep(SLEEP_DURATION);
+    }
+
+    {
+      const url =
+        __ENV.css_api_url + `/integrations/${integrationId}/${__ENV.environment}/users/${__ENV.username}/roles`;
+      const requestOptions = Object.assign({}, options);
+      requestOptions.headers.Accept = 'application/json';
+      const body = [{ name: 'role-mapping' }];
+      const response = http.post(url, JSON.stringify(body), requestOptions);
+
+      console.debug(`Response from CSS API: ${JSON.stringify(response, 0, 2)}`);
+
+      check(response, {
+        'should return 201 when success': (r) => r.status === 201,
+        'return role name': (r) => r.json().data.find((role) => role.name === 'role-mapping'),
+      });
+
+      sleep(SLEEP_DURATION);
+    }
+  });
+
+  group('GET users associated to a role', () => {
+    {
+      const url = __ENV.css_api_url + `/integrations/${integrationId}/${__ENV.environment}/roles/nonexistentrole/users`;
+      const response = http.get(url, options);
+
+      console.debug(`Response from CSS API: ${JSON.stringify(response, 0, 2)}`);
+
+      check(response, {
+        'should return 404 when role name does not exist': (r) => r.status === 404,
+      });
+
+      sleep(SLEEP_DURATION);
+    }
+    {
+      const url =
+        __ENV.css_api_url +
+        `/integrations/${integrationId}/${__ENV.environment}/roles/role-mapping/users?page=1&max=51`;
+      const response = http.get(url, options);
+
+      console.debug(`Response from CSS API: ${JSON.stringify(response, 0, 2)}`);
+
+      check(response, {
+        'should return 400 when max count is above 50': (r) => r.status === 400,
+      });
+
+      sleep(SLEEP_DURATION);
+    }
+    {
+      const url = __ENV.css_api_url + `/integrations/${integrationId}/${__ENV.environment}/roles/role-mapping/users`;
+      const response = http.get(url, options);
+
+      console.debug(`Response from CSS API: ${JSON.stringify(response, 0, 2)}`);
+
+      check(response, {
+        'should return 200 when success': (r) => r.status === 200,
+        'should return page': (r) => r.json().page === 1,
+        'should return username': (r) => r.json().data.find((user) => user.username === __ENV.username),
+      });
+
+      sleep(SLEEP_DURATION);
+    }
+
+    {
+      const url =
+        __ENV.css_api_url + `/integrations/${integrationId}/${__ENV.environment}/roles/role-mapping/users?page=1&max=1`;
+      const response = http.get(url, options);
+
+      console.debug(`Response from CSS API: ${JSON.stringify(response, 0, 2)}`);
+
+      check(response, {
+        'should return 200 when success': (r) => r.status === 200,
+        'should return page': (r) => r.json().page === 1,
+        'return username': (r) => r.json().data.find((user) => user.username === __ENV.username),
+      });
+
+      sleep(SLEEP_DURATION);
+    }
+  });
+
+  group('GET roles associated to an user', () => {
+    {
+      const url = __ENV.css_api_url + `/integrations/${integrationId}/${__ENV.environment}/users/nonexistentuser/roles`;
+      const response = http.get(url, options);
+
+      console.debug(`Response from CSS API: ${JSON.stringify(response, 0, 2)}`);
+
+      check(response, {
+        'should return 400 when invalid or no idp passed': (r) => r.status === 400,
+      });
+
+      sleep(SLEEP_DURATION);
+    }
+    {
+      const url =
+        __ENV.css_api_url + `/integrations/${integrationId}/${__ENV.environment}/users/nonexistentuser@idir/roles`;
+      const response = http.get(url, options);
+
+      console.debug(`Response from CSS API: ${JSON.stringify(response, 0, 2)}`);
+
+      check(response, {
+        'should return 404 when valid idp passed but user not found': (r) => r.status === 404,
+      });
+
+      sleep(SLEEP_DURATION);
+    }
+    {
+      const url =
+        __ENV.css_api_url + `/integrations/${integrationId}/${__ENV.environment}/users/${__ENV.username}/roles`;
+      const response = http.get(url, options);
+
+      console.debug(`Response from CSS API: ${JSON.stringify(response, 0, 2)}`);
+
+      check(response, {
+        'should return 200 when success': (r) => r.status === 200,
+        'should return role name': (r) => r.json().data.find((role) => role.name === 'role-mapping'),
+      });
+
+      sleep(SLEEP_DURATION);
+    }
+  });
+
+  group('DELETE unassign role from an user', () => {
+    {
+      const url =
+        __ENV.css_api_url +
+        `/integrations/${integrationId}/${__ENV.environment}/users/${__ENV.username}/roles/role-mapping?param1=1&param2=2`;
+      const response = http.del(url, null, options);
+
+      console.debug(`Response from CSS API: ${JSON.stringify(response, 0, 2)}`);
+
+      check(response, {
+        'should return 400 when arbitrary query params passed': (r) => r.status === 400,
+      });
+
+      sleep(SLEEP_DURATION);
+    }
+
+    {
+      const url =
+        __ENV.css_api_url +
+        `/integrations/${integrationId}/${__ENV.environment}/users/${__ENV.username}/roles/role-mapping00000`;
+      const response = http.del(url, null, options);
+
+      console.debug(`Response from CSS API: ${JSON.stringify(response, 0, 2)}`);
+
+      check(response, {
+        'should return 400 when non-associated role name passed': (r) => r.status === 400,
+      });
+
+      sleep(SLEEP_DURATION);
+    }
+    {
+      const url =
+        __ENV.css_api_url +
+        `/integrations/${integrationId}/${__ENV.environment}/users/nonexistentuser/roles/role-mapping`;
+
+      const response = http.del(url, null, options);
+
+      console.debug(`Response from CSS API: ${JSON.stringify(response, 0, 2)}`);
+
+      check(response, {
+        'should return 400 when username with invalid or no idp passed': (r) => r.status === 400,
+      });
+
+      sleep(SLEEP_DURATION);
+    }
+    {
+      const url =
+        __ENV.css_api_url +
+        `/integrations/${integrationId}/${__ENV.environment}/users/nonexistentuser@idir/roles/role-mapping`;
+
+      const response = http.del(url, null, options);
+
+      console.debug(`Response from CSS API: ${JSON.stringify(response, 0, 2)}`);
+
+      check(response, {
+        'should return 404 when non-existent username with valid idp passed': (r) => r.status === 404,
+      });
+
+      sleep(SLEEP_DURATION);
+    }
+
+    {
+      const url =
+        __ENV.css_api_url +
+        `/integrations/${integrationId}/${__ENV.environment}/users/${__ENV.username}/roles/role-mapping`;
+
+      const response = http.del(url, null, options);
+
+      console.debug(`Response from CSS API: ${JSON.stringify(response, 0, 2)}`);
+
+      check(response, {
+        'should return 204 when success': (r) => r.status === 204,
+      });
+
+      sleep(SLEEP_DURATION);
+    }
+  });
+
   group('cleanup', () => {
     {
       // delete role
--- lambda/css-api/src/ajv-validator.ts
+++ lambda/css-api/src/ajv-validator.ts
@@ -1,7 +1,7 @@
 import ajv from 'ajv';
 import ajvErrors from 'ajv-errors';
 
-const validator = new ajv({ allErrors: true });
+const validator = new ajv({ allErrors: true, coerceTypes: true });
 
 ajvErrors(validator);
 
