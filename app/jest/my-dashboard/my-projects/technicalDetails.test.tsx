a566e53daf96a097b72f55af27c5ca363cb1031f
ZHE CHEN <kzsyixi@gmail.com>
2023-01-18 16:15:54 -0800

    chore: rearrange folder structure; update test file naming

--- app/jest/my-dashboard/my-projects/technicalDetails.test.tsx
+++ app/jest/my-dashboard/my-projects/technicalDetails.test.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import { render, screen, fireEvent, waitFor, within } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor, within, cleanup } from '@testing-library/react';
 import IntegrationInfoTabs from 'page-partials/my-dashboard/IntegrationInfoTabs';
+import SubmittedStatusIndicator from 'components/SubmittedStatusIndicator';
 import { getInstallation } from 'services/keycloak';
 import { prettyJSON, copyTextToClipboard, downloadText } from 'utils/text';
+import { sampleRequest } from '../../samples/integrations';
+import type { Status } from 'interfaces/types';
 
 type Text = string | RegExp;
 const expectText = (text: Text) => expect(screen.getByText(text)).toBeTruthy();
@@ -256,5 +259,41 @@ describe('Applied Status header, button and link test', () => {
     fireEvent.click(screen.getByRole('button', { name: 'Download' }));
     expect(getInstallation).toHaveBeenCalled();
     //await waitFor(() => { expect(screen.findByText('Installation copied to clipboard')).toBeInTheDocument(); });
+  });
+});
+
+describe('Submission Status Indicator', () => {
+  it('Should show the planned message', () => {
+    render(<SubmittedStatusIndicator integration={{ ...sampleRequest, status: 'planned' as Status }} />);
+    expect(screen.getByText('Terraform plan succeeded...'));
+  });
+  it('Should show the submitted message', () => {
+    render(<SubmittedStatusIndicator integration={{ ...sampleRequest, status: 'submitted' as Status }} />);
+    expect(screen.getByText('Process request submitted...'));
+  });
+  it('Should show the created message', () => {
+    render(<SubmittedStatusIndicator integration={{ ...sampleRequest, status: 'pr' as Status }} />);
+    expect(screen.getByText('Pull request created...'));
+  });
+  it('Should show the error message', () => {
+    render(<SubmittedStatusIndicator integration={{ ...sampleRequest, status: 'prFailed' as Status }} />);
+    expect(screen.getByText('An error has occurred'));
+    cleanup();
+
+    render(<SubmittedStatusIndicator integration={{ ...sampleRequest, status: 'planFailed' as Status }} />);
+    expect(screen.getByText('An error has occurred'));
+    cleanup();
+
+    render(<SubmittedStatusIndicator integration={{ ...sampleRequest, status: 'applyFailed' as Status }} />);
+    expect(screen.getByText('An error has occurred'));
+    cleanup();
+  });
+  it('Should show the last updated date', () => {
+    render(
+      <SubmittedStatusIndicator
+        integration={{ ...sampleRequest, status: 'pr' as Status, updatedAt: '2021-08-20T18:50:04.115Z' }}
+      />,
+    );
+    expect(screen.getByText(`Last updated at ${new Date('2021-08-20T18:50:04.115Z').toLocaleString()}`));
   });
 });
