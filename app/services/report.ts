f8b935d8e61e2ba3c2e92d56d8254d0e1ca331f8
ZHE CHEN <kzsyixi@gmail.com>
2023-03-28 10:53:32 -0700

    chore: update stmt for naming excel sheet

--- app/services/report.ts
+++ app/services/report.ts
@@ -8,12 +8,9 @@ export const downloadAllStandardIntegrationsReport = async (): Promise<void | [n
     const result = await instance.get('reports/all-standard-integrations').then((res) => res.data);
 
     const workSheet = XLSX.utils.json_to_sheet(result);
-    const workBook = {
-      Sheets: {
-        data: workSheet,
-      },
-      SheetNames: ['All standard integration'],
-    };
+    const workBook = XLSX.utils.book_new();
+
+    XLSX.utils.book_append_sheet(workBook, workSheet, 'All standard integrations');
     XLSX.writeFile(workBook, 'All Standard Integrations.xlsx');
   } catch (err: any) {
     console.log(err);
