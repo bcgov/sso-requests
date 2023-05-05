9df113054ffc622ed3b7aafc1232c7b159076919
thegentlemanphysicist <jon.w.sharman@gmail.com>
2023-05-05 11:06:38 -0700

    Revert "chore: disable the project name change frontend (#953)"
    This reverts commit ed4b96729f650cbce5a26e8520c7a7070ff4aac4.

--- app/schemas-ui/ui.ts
+++ app/schemas-ui/ui.ts
@@ -21,7 +21,6 @@ const getUISchema = ({ integration, isAdmin }: Props) => {
   return {
     projectName: {
       'ui:placeholder': 'Project Name',
-      'ui:readonly': !isNew,
     },
     usesTeam: {
       'ui:widget': 'radio',
