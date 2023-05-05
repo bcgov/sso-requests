ee1f58f422fb4c5261667c7f7703d4c86365b771
Jonathan Sharman <jon.w.sharman@gmail.com>
2023-05-05 11:58:14 -0700

    chore: Disable project name editing again

--- app/schemas-ui/ui-gold.ts
+++ app/schemas-ui/ui-gold.ts
@@ -70,7 +70,7 @@ const getUISchema = ({ integration, formData, isAdmin }: Props) => {
     projectName: {
       'ui:placeholder': 'Project Name',
       classNames: 'short-field-string',
-      'ui:readonly': !isNew && formData?.protocol === 'saml' && formData?.status !== 'draft',
+      'ui:readonly': !isNew && formData?.status !== 'draft',
     },
     teamId: {
       classNames: 'short-field-string',
