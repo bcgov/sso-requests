c936c75afba56ca00bf37cd71f6601980c4f7307
Nithin Shekar Kuruba <81444731+NithinKuruba@users.noreply.github.com>
2023-07-19 13:00:05 -0700

    feat: new endpoint to remove inactive users (#994)
    * feat: new endpoint to delete inactive users
    
    * fix: return 404 if user is not found
    
    * feat: added a unit test to validate delete stale users
    
    * fix: handled more edge cases when removing inactive users
    
    * fix: added await for destroy command
    
    * feat: use team id to get team integrations
    
    * fix: used teamId instead of id

--- lambda/__tests__/jest.setup.js
+++ lambda/__tests__/jest.setup.js
@@ -1,6 +1,7 @@
 const { createImportSpecifier } = require('typescript');
 const { sequelize, models } = require('../shared/sequelize/models/models');
 process.env.GH_SECRET = 'test';
+process.env.API_AUTH_SECRET = 'test';
 
 afterAll(async () => {
   return sequelize.close();
