1193a5f00bea220ae53fe3e02b6bd8490ddecc32
Jon Langlois <jonathan-langlois@live.ca>
2023-10-10 09:51:36 -0700

    chore: backend for survey add comments for where to update the backend

--- lambda/app/src/routes.ts
+++ lambda/app/src/routes.ts
@@ -168,6 +168,7 @@ export const setRoutes = (app: any) => {
   app.get(`/me`, async (req, res) => {
     try {
       const integrations = await findMyOrTeamIntegrationsByService(req.user.id);
+      // TODO: Include survey submissions on profile once exists in DB.
       res.status(200).json({ ...req.user, integrations });
     } catch (err) {
       handleError(res, err);
