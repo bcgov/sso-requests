8bd109214260d13adceba7ec933a4aed56eccea4
Jon Langlois <jonathan-langlois@live.ca>
2023-10-12 16:45:13 -0700

    feat: email
    send emails when submit survey

---
+++ lambda/shared/templates/survey-completed-notification/index.ts
@@ -0,0 +1,43 @@
+import * as fs from 'fs';
+import Handlebars = require('handlebars');
+import { sendEmail } from '@lambda-shared/utils/ches';
+import { SSO_EMAIL_ADDRESS } from '@lambda-shared/local';
+import { User } from '@lambda-shared/interfaces';
+import { processUser } from '../helpers';
+import { EMAILS } from '@lambda-shared/enums';
+import type { RenderResult } from '../index';
+import { UserSurveyInformation } from 'app/interfaces/team';
+
+const SUBJECT_TEMPLATE = `Survey Completed`;
+const template = fs.readFileSync(__dirname + '/survey-completed-notification.html', 'utf8');
+
+const subjectHandler = Handlebars.compile(SUBJECT_TEMPLATE, { noEscape: true });
+const bodyHandler = Handlebars.compile(template, { noEscape: true });
+
+interface DataProps {
+  user: User;
+  public: boolean;
+  message: string;
+  rating: number;
+  triggerEvent: keyof UserSurveyInformation;
+}
+
+export const render = async (originalData: DataProps): Promise<RenderResult> => {
+  const { user } = originalData;
+  const data = { ...originalData, user: await processUser(user) };
+  return {
+    subject: subjectHandler(data),
+    body: bodyHandler(data),
+  };
+};
+
+export const send = async (data: DataProps, rendered: RenderResult) => {
+  return sendEmail({
+    code: EMAILS.REQUEST_LIMIT_EXCEEDED,
+    to: [data.public ? data.user.idirEmail : SSO_EMAIL_ADDRESS],
+    cc: [],
+    ...rendered,
+  });
+};
+
+export default { render, send };
