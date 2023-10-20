bb7ee32cb08ec2c6a79e76456cbbfd25e2ad2695
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2023-10-20 15:14:36 -0700

    fix: fixed terraform generated error iteration 07

--- terraform/roles.tf
+++ terraform/roles.tf
@@ -23,7 +23,8 @@ resource "aws_iam_role" "ecs_sso_grafana_task_execution_role" {
 
 # Attaching task execution and read from RDS policies to task execution role
 resource "aws_iam_role_policy_attachment" "ecs_sso_grafana_task_role_policy_attachment" {
-  role = aws_iam_role.ecs_sso_grafana_task_execution_role.name
+  depends_on = [aws_iam_policy.secrets_manager_read_policy]
+  role       = aws_iam_role.ecs_sso_grafana_task_execution_role.name
   for_each = toset([
     "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy",
     "arn:aws:iam::aws:policy/AmazonRDSReadOnlyAccess",
