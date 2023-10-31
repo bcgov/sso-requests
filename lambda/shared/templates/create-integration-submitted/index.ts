2061e6abaf87ea07bd75e78b597fee386844e609
jonathan langlois <jonathan.langlois@gov.bc.ca>
2023-10-31 15:01:39 -0700

    chore: sonarcloud
    sonarcloud rearrange

--- lambda/shared/templates/create-integration-submitted/index.ts
+++ lambda/shared/templates/create-integration-submitted/index.ts
@@ -3,7 +3,7 @@ import Handlebars = require('handlebars');
 import { processRequest } from '../helpers';
 import { IntegrationData } from '@lambda-shared/interfaces';
 import { sendEmail } from '@lambda-shared/utils/ches';
-import { SSO_EMAIL_ADDRESS, IDIM_EMAIL_ADDRESS, OCIO_EMAIL_ADDRESS, DIT_EMAIL_ADDRESS } from '@lambda-shared/local';
+import { SSO_EMAIL_ADDRESS, IDIM_EMAIL_ADDRESS, DIT_EMAIL_ADDRESS, OCIO_EMAIL_ADDRESS } from '@lambda-shared/local';
 import { getIntegrationEmails } from '../helpers';
 import { EMAILS } from '@lambda-shared/enums';
 import { usesBceid, usesGithub, usesVerifiableCredential } from '@app/helpers/integration';
@@ -38,7 +38,9 @@ export const send = async (data: DataProps, rendered: RenderResult) => {
   const cc = [SSO_EMAIL_ADDRESS];
   if (usesBceid(integration)) cc.push(IDIM_EMAIL_ADDRESS);
   if (usesGithub(integration)) cc.push(OCIO_EMAIL_ADDRESS);
-  if (usesVerifiableCredential(integration)) cc.push(DIT_EMAIL_ADDRESS);
+  if (usesVerifiableCredential(integration)) {
+    cc.push(DIT_EMAIL_ADDRESS);
+  }
 
   return sendEmail({
     code: EMAILS.CREATE_INTEGRATION_SUBMITTED,
