64b35ee36b4d7c8a0a4b7d2093ee7e8b3948193e
Jonathan Langlois <37274633+jlangy@users.noreply.github.com>
2023-12-12 14:59:25 -0800

    chore: change grafana api route  (#1096)
    * chore: backend
    
    create backend route for logs
    
    * fix: sonar
    
    add recomended sonar fixes
    
    * fix: sonar
    
    more sonarcloud fixes
    
    * chore: refactor
    
    move validation logic to controller
    
    * chore: sonar
    
    remove unused import
    
    * chore: api route
    
    change grafana endpoint
    
    * chore: remove uid
    
    fetch uid on all requests instead of env var
    
    * chore: refactor
    
    factor out uid fetch to a function

--- lambda/app/src/grafana.ts
+++ lambda/app/src/grafana.ts
@@ -1,20 +1,50 @@
 import axios from 'axios';
 
+export const fetchDatasourceUID = async (datasourceName) => {
+  return axios
+    .get(`${process.env.GRAFANA_API_URL}/datasources`, {
+      headers: {
+        Authorization: `Bearer ${process.env.GRAFANA_API_TOKEN}`,
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    })
+    .then((res) => res.data.find((source) => source.name === datasourceName)?.uid);
+};
+
 export const queryGrafana = async (query: string, start: number, end: number, limit: number) => {
-  const results = await axios.get(`${process.env.SSO_LOGS_URL}/v1/query_range`, {
-    params: {
-      query,
-      start,
-      end,
-      limit,
+  const lokiUID = await fetchDatasourceUID('SSO Loki');
+  const response = await axios.post(
+    `${process.env.GRAFANA_API_URL}/ds/query`,
+    {
+      queries: [
+        {
+          refId: 'logs',
+          expr: query,
+          queryType: 'range',
+          datasource: {
+            type: 'loki',
+            uid: lokiUID,
+          },
+          maxLines: limit,
+        },
+      ],
+      from: String(start),
+      to: String(end),
     },
-    headers: {
-      Authorization: `Bearer ${process.env.GRAFANA_API_TOKEN}`,
+    {
+      params: {
+        ds_type: 'loki',
+      },
+      headers: {
+        Authorization: `Bearer ${process.env.GRAFANA_API_TOKEN}`,
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
     },
-  });
-  return results.data.data.result.reduce((acc, curr) => {
-    // Returning just the log, id in first position
-    const currentLogs = curr.values.map((val) => val[1]);
-    return acc.concat(currentLogs);
+  );
+  return response.data.results.logs.frames.reduce((acc, curr) => {
+    // Raw logs are in third array position. First entries are filenames and IDs
+    return acc.concat(curr.data.values[2]);
   }, []);
 };
--- terraform/lambda-app.tf
+++ terraform/lambda-app.tf
@@ -51,8 +51,8 @@ resource "aws_lambda_function" "app" {
       CHES_PASSWORD                  = var.ches_password
       CHES_USERNAME                  = var.ches_username
       INCLUDE_DIGITAL_CREDENTIAL     = var.include_digital_credential
-      SSO_LOGS_URL                   = var.sso_logs_url
       GRAFANA_API_TOKEN              = var.grafana_api_token
+      GRAFANA_API_URL                = var.grafana_api_url
     }
   }
 
