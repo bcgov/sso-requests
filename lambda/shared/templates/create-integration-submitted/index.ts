64b16d9fb3d869d4ffcfac9a2d42e66a413f4130
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2024-01-05 16:41:16 -0800

    feat: added numbering to emails

--- lambda/shared/templates/create-integration-submitted/index.ts
+++ lambda/shared/templates/create-integration-submitted/index.ts
@@ -9,7 +9,7 @@ import { EMAILS } from '@lambda-shared/enums';
 import { usesBceid, usesGithub, usesDigitalCredential } from '@app/helpers/integration';
 import type { RenderResult } from '../index';
 
-const SUBJECT_TEMPLATE = `Pathfinder SSO request submitted & additional important information`;
+const SUBJECT_TEMPLATE = `Pathfinder SSO request submitted & additional important information (email 1 of 2)`;
 const template = fs.readFileSync(__dirname + '/create-integration-submitted.html', 'utf8');
 
 const subjectHandler = Handlebars.compile(SUBJECT_TEMPLATE, { noEscape: true });
