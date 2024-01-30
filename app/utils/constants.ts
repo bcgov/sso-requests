0eade8785f8ab0ec06c359f0832bba62f80935e3
jonathan langlois <jonathan.langlois@gov.bc.ca>
2024-01-30 10:05:24 -0800

    chore: initials
    change full names to initials

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -79,7 +79,7 @@ export const testimonials: ITestimonial[] = [
     id: 1,
     title: 'Exceptional Customer Service',
     author: {
-      name: 'Wim Mulder',
+      name: 'W.M.',
       title: 'Senior Technical Analyst, IM/IT Enterprise Projects',
     },
     body: 'The Common Hosted Single Sign-On (CSS) service has streamlined login access for both Government staff and contractors to our Snow Avalanche Weather System (SAWSx), with its easy integration requiring minimal code. The team provides exceptional customer service, swiftly addressing a critical issue on our end. Grateful for the prompt assistance and seamless experience provided by the team.',
@@ -89,7 +89,7 @@ export const testimonials: ITestimonial[] = [
     id: 2,
     title: 'Self Service: Configure Dev and Test',
     author: {
-      name: 'Aditya Sharma',
+      name: 'A.S.',
       title: 'Quality Assurance Lead',
     },
     body: "It was a breeze to integrate our SAAS platform, BrowserStack, with Single Sign-On with the expert assistance of the SSO team.  The CSS app's flexibility eased integration challenges, allowing seamless Dev and Test setups. The responsive support received, especially in overcoming signed assertion hurdles, showcased the team's expertise and contributed to a smooth experience, noticeably enhancing our login process.",
@@ -99,7 +99,7 @@ export const testimonials: ITestimonial[] = [
     id: 3,
     title: 'Stable Service and Super Support',
     author: {
-      name: 'Gary Wong',
+      name: 'G.W.',
       title: 'Senior Product Owner',
     },
     body: 'Having collaborated with the CSS team across multiple projects, their responsiveness to technical inquiries and support during onboarding has been invaluable. The user-friendly CSS Dashboard, technical support via RocketChat, and regular open demo sessions have greatly contributed to our collaborative community. With acknowledgment within 15 minutes and resolutions ranging from 15 minutes to the same day, the service has been consistently solid.',
@@ -109,7 +109,7 @@ export const testimonials: ITestimonial[] = [
     title: 'Really Positive Experience',
     id: 4,
     author: {
-      name: 'Jessica Wade',
+      name: 'J.W.',
       title: 'Senior Product Manager, Digital Delivery',
     },
     body: 'Our team has had a really positive experience working with the single-sign on keycloak service. We were able to work with the team to get us set up in a custom realm which suited the needs at BC Parks. Since then we have been able to integrate BCeID and BC Services Card and are managing roles for up to 5 different products through the tool and enabled easy log ins for our users so they can see what is relevant to them.',
