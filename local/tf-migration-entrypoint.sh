f6a5754ed63e7c8bf8bbfe6e8b057bb65e12ae74
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2024-02-07 14:44:19 -0800

    feat: use JAVA_OPTS_APPEND instead of command

--- local/tf-migration-entrypoint.sh
+++ local/tf-migration-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-while [ ! $(curl -s -o /dev/null -w "%{http_code}" http://dev-keycloak:9080/auth/realms/master) -eq 200 ]; do
+while [ ! $(curl -s -o /dev/null -w "%{http_code}" "$TF_VAR_keycloak_url/auth/realms/master") -eq 200 ]; do
   sleep 5
   echo "keycloak is not up yet"
 done
