c34031aa3b85ca54c742b5672124f40856279320
Jonathan Sharman <jon.w.sharman@gmail.com>
2024-02-13 11:45:02 -0800

    chore: remover the print statement

--- local/tf-migration-entrypoint.sh
+++ local/tf-migration-entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 IP=$(ping -c 1 dev-keycloak | grep 'PING' | awk '{print $3}' | tr -d '()')
-echo $IP 9080/auth/realms/master
 while [ ! $(curl -s -o /dev/null -w "%{http_code}" "${IP}9080/auth/realms/master") -eq 200 ]; do
   sleep 5
   IP=$(ping -c 1 dev-keycloak | grep 'PING' | awk '{print $3}' | tr -d '()')
