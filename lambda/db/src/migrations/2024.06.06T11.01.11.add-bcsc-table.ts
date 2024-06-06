a9cb1557d2e501199af61f903c8ca3f5f63005ad
jonathan langlois <jonathan.langlois@gov.bc.ca>
2024-06-06 13:02:49 -0700

    chore: migration
    add migration for bcsc clients table

---
+++ lambda/db/src/migrations/2024.06.06T11.01.11.add-bcsc-table.ts
@@ -0,0 +1,80 @@
+import { DataTypes } from 'sequelize';
+
+export const name = '2024.06.06T11.01.11.add-bcsc-table';
+
+// see https://sequelize.org/master/manual/naming-strategies.html
+export const up = async ({ context: sequelize }) => {
+  await sequelize.getQueryInterface().createTable('bcsc_client_registrations', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      defaultValue: sequelize.UUIDV4,
+      autoIncrement: true,
+    },
+    client_id: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    client_secret: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    registration_access_token: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    privacy_zone_uri: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    client_name: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    client_uri: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    claims: {
+      type: DataTypes.ARRAY(DataTypes.TEXT),
+      allowNull: false,
+      defaultValue: [],
+    },
+    scopes: {
+      type: DataTypes.ARRAY(DataTypes.TEXT),
+      allowNull: false,
+      defaultValue: [],
+    },
+    created: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      field: 'created_at',
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      field: 'updated_at',
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+  });
+
+  await sequelize.getQueryInterface().addColumn('requests', 'bcsc_client_id', {
+    type: DataTypes.INTEGER,
+    references: { model: 'bcsc_client_registrations', key: 'id' },
+    allowNull: true,
+  });
+};
+
+export const down = async ({ context: sequelize }) => {
+  await sequelize.getQueryInterface().removeColumn('requests', 'bcsc_client_id');
+  await sequelize.getQueryInterface().dropTable('bcsc_client_registrations');
+};
+
+export default { name, up, down };
