9dba4c837e8f031a37e3a194a542210e58eda46c
Jonathan Langlois <37274633+jlangy@users.noreply.github.com>
2024-09-17 14:14:26 -0700

    feat: role message (#1288)
    * fix: padding
    
    remove unecessary error padding
    
    * chore: role warning
    
    add warning for bcsc and dc role assignment
    
    * chore: asterisk
    
    add asterisk to note messages

--- app/schemas/providers-gold.ts
+++ app/schemas/providers-gold.ts
@@ -122,6 +122,8 @@ export default function getSchema(
         enum: idpEnum,
         enumNames: idpEnumNames,
       },
+      warningMessage:
+        'Role assignment is not available for the BC Services Card and Digital Credential Identity Providers.',
       tooltips: idpEnum.map((idp) => {
         if (idp === 'azureidir') {
           return {
