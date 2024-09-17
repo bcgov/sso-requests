bc65c81fd56c44b29f13e0cc0eaa96e739460c78
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2024-09-17 09:02:22 -0700

    fix: unit tests

--- lambda/shared/local.ts
+++ lambda/shared/local.ts
@@ -8,4 +8,4 @@ export const OCIO_EMAIL_ADDRESS = 'bcgov.sso@gov.bc.ca';
 export const DIT_EMAIL_ADDRESS = 'ditrust@gov.bc.ca';
 
 export const DIT_ADDITIONAL_EMAIL_ADDRESS =
-  process.env.APP_ENV === 'production' ? 'aaron.unger@gov.bc.ca' : 'nithinshekar.kuruba@gov.bc.ca';
+  process.env.APP_ENV === 'production' ? 'aaron.unger@gov.bc.ca' : 'bcgov.sso@gov.bc.ca';
