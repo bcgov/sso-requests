c0515deeca54e842c6a9bb4a717b88f696fe94e8
jonathan langlois <jonathan.langlois@gov.bc.ca>
2024-09-24 14:30:03 -0700

    test: ci
    test in ci pipeline

--- app/cypress/appActions/Request.ts
+++ app/cypress/appActions/Request.ts
@@ -588,7 +588,7 @@ class Request {
         cy.get(this.reqPage.deleteButton).eq(index).scrollIntoView().click({ force: true });
         cy.wait(1000);
         this.reqPage.confirmDeleteIntegration(id, projectName);
-        cy.wait(1000);
+        cy.wait(5000);
         cy.log('Delete Request: ' + id.toString());
       }
     });
