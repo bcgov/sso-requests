5fb56c4e426fec827c63ba3cd829c9f95a9e7291
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2024-10-01 09:59:59 -0700

    fix: refetch events when IDP status is approved

--- app/page-partials/admin-dashboard/AdminTabs/TabContent.tsx
+++ app/page-partials/admin-dashboard/AdminTabs/TabContent.tsx
@@ -62,7 +62,7 @@ function TabContent({ integration, type, canApproveProd, awaitingTFComplete, onA
 
   useEffect(() => {
     getApprovalEvents();
-  }, [integration?.id]);
+  }, [integration?.id, integration?.bceidApproved, integration?.githubApproved, integration?.bcServicesCardApproved]);
 
   if (!integration) return null;
 
