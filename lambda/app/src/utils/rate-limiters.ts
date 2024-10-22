c7d1e2110b9b6ceda7585b7ac15a2a55b23844c0
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2024-10-22 13:34:06 -0700

    fix: handle database configuration

--- lambda/app/src/utils/rate-limiters.ts
+++ lambda/app/src/utils/rate-limiters.ts
@@ -16,10 +16,10 @@ export const logsRateLimiter = rateLimit({
   keyGenerator: (req) => getClientIp(req),
   store: new PostgresStore(
     {
-      user: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      host: process.env.DB_HOSTNAME,
-      database: process.env.DB_NAME,
+      user: process.env.DB_USERNAME || '',
+      password: process.env.DB_PASSWORD || '',
+      host: process.env.DB_HOSTNAME || '',
+      database: process.env.DB_NAME || '',
       port: 5432,
       ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
     },
