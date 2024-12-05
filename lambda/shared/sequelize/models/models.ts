159633b5aea6034f8d1c2f25ea75f09910eb7eed
jonathan langlois <jonathan.langlois@gov.bc.ca>
2024-12-05 11:58:54 -0800

    chore: revert models
    revert change to other model imports

--- lambda/shared/sequelize/models/models.ts
+++ lambda/shared/sequelize/models/models.ts
@@ -17,30 +17,26 @@ export const models: any = {};
 export const modelNames: string[] = [];
 export let sequelize;
 
-export const loadSequelize = () => {
-  if (config.databaseUrl) {
-    sequelize = new Sequelize(config.databaseUrl, config);
-  } else if (config.use_env_variable && process.env[config.use_env_variable]) {
-    sequelize = new Sequelize(process.env[config.use_env_variable], config);
-  } else {
-    sequelize = new Sequelize(config.database, config.username, config.password, config);
+if (config.databaseUrl) {
+  sequelize = new Sequelize(config.databaseUrl, config);
+} else if (config.use_env_variable && process.env[config.use_env_variable]) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+console.log('sequelize initialized', !!sequelize);
+
+[Event, Request, Team, User, UserTeam, Survey, RequestQueue, RequestRole, BcscClient].forEach((init) => {
+  const model = init(sequelize, DataTypes);
+  models[model.name] = model;
+  modelNames.push(model.name);
+});
+
+Object.keys(models).forEach((modelName) => {
+  if (models[modelName]?.options.associate) {
+    models[modelName].options.associate(models);
   }
-
-  console.log('sequelize initialized', !!sequelize);
-
-  [Event, Request, Team, User, UserTeam, Survey, RequestQueue, RequestRole, BcscClient].forEach((init) => {
-    const model = init(sequelize, DataTypes);
-    models[model.name] = model;
-    modelNames.push(model.name);
-  });
-
-  Object.keys(models).forEach((modelName) => {
-    if (models[modelName]?.options.associate) {
-      models[modelName].options.associate(models);
-    }
-  });
-};
-
-loadSequelize();
+});
 
 export default { models, modelNames, sequelize };
