438eafb6847cac46bc6780095af97be0cdc667c0
jonathan langlois <jonathan.langlois@gov.bc.ca>
2025-04-25 11:17:32 -0700

    chore: navigation
    update navigation to only click if on wrong url

--- app/cypress/appActions/Navigation.ts
+++ app/cypress/appActions/Navigation.ts
@@ -1,15 +1,25 @@
 class Navigation {
   goToMyDashboard() {
-    cy.get('header a[href="/my-dashboard"]').click();
+    cy.url().then((url) => {
+      if (!url.endsWith('/my-dashboard')) cy.get('header a[href="/my-dashboard"]').click();
+    });
   }
 
   goToMyTeams() {
-    cy.get('header a[href="/my-dashboard"]').click();
-    cy.contains('My Teams').click();
+    cy.url().then((url) => {
+      if (url.endsWith('/my-dashboard/teams')) return;
+      if (url.endsWith('/my-dashboard')) cy.contains('My Teams').click();
+      else {
+        cy.get('header a[href="/my-dashboard"]').click();
+        cy.contains('My Teams').click();
+      }
+    });
   }
 
   goToAdminDashboard() {
-    cy.get('header a[href="/admin-dashboard"]').click();
+    cy.url().then((url) => {
+      if (!url.endsWith('/admin-dashboard')) cy.get('header a[href="/admin-dashboard"]').click();
+    });
   }
 }
 
