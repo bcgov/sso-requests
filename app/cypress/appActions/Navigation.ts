05ca4b11df160bc796faa61b0c61982d34e8f817
Jonathan Langlois <37274633+jlangy@users.noreply.github.com>
2025-05-02 16:15:17 -0700

    chore: cypress tests (#1421)
    * chore: disable social
    
    disable social idp option in sandbox
    
    * fix: key warning
    
    add key to loader component
    
    * chore: flaky selectors
    
    remove auto-generated ids from cypress selectors
    
    * chore: remove social
    
    remove social label from idp list
    
    * fix: cypress
    
    update to include optional base path for deployed env
    
    * chore: env var
    
    use localtest env var to determine path
    
    * chore: upload videos
    
    upload videos to debug tests
    
    * chore: navigation in cypress
    
    ensure full page load before continuing
    
    * chore: check loader exists
    
    ensure loader loads before checking dissapearance
    
    * chore: remove videos
    
    comment out video uploads

--- app/cypress/appActions/Navigation.ts
+++ app/cypress/appActions/Navigation.ts
@@ -1,25 +1,38 @@
 class Navigation {
+  basePath: string = Cypress.env('localtest') ? '' : '/sso-requests-sandbox';
+  waitForPageLoad() {
+    // Wait for loader to appear and clear to ensure full page load
+    cy.get('[data-testid="grid-loading"]').should('exist');
+    cy.get('[data-testid="grid-loading"]').should('not.exist');
+  }
+
   goToMyDashboard() {
     cy.url().then((url) => {
       cy.log(url);
-      if (!url.includes('/my-dashboard')) cy.get('header a[href="/my-dashboard"]').click();
+      if (!url.includes('/my-dashboard') || url.includes('/teams')) {
+        cy.get(`header a[href="${this.basePath}/my-dashboard"]`).click();
+        this.waitForPageLoad();
+      }
     });
   }
 
   goToMyTeams() {
     cy.url().then((url) => {
       if (url.includes('/my-dashboard/teams')) return;
-      if (url.includes('/my-dashboard') && !url.includes('/teams')) cy.contains('My Teams').click();
-      else {
-        cy.get('header a[href="/my-dashboard"]').click();
+      if (url.includes('/my-dashboard') && !url.includes('/teams')) {
         cy.contains('My Teams').click();
+        this.waitForPageLoad();
+      } else {
+        cy.get(`header a[href="${this.basePath}/my-dashboard"]`).click();
+        cy.contains('My Teams').click();
+        this.waitForPageLoad();
       }
     });
   }
 
   goToAdminDashboard() {
     cy.url().then((url) => {
-      if (!url.endsWith('/admin-dashboard')) cy.get('header a[href="/admin-dashboard"]').click();
+      if (!url.endsWith('/admin-dashboard')) cy.get(`header a[href="${this.basePath}/admin-dashboard"]`).click();
     });
   }
 }
