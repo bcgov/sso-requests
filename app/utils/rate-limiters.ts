a9987c2aac416c299ff9846be0ecc8ef35547ec1
Jonathan Sharman <jon.w.sharman@gmail.com>
2025-05-28 11:00:42 -0700

    chore: add redis to helm, reconect redis in app

--- app/utils/rate-limiters.ts
+++ app/utils/rate-limiters.ts
@@ -1,6 +1,7 @@
 import rateLimit from 'express-rate-limit';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { RequestHandler } from 'next/dist/server/next';
+import RedisStore from 'rate-limit-redis';
 
 const getClientIp = (req: any) => {
   const id = req.query?.id ?? req.query?.integrationId;
@@ -21,13 +22,28 @@ function initMiddleware(
     });
 }
 
-export const logsRateLimiter = initMiddleware(
-  rateLimit({
-    windowMs: 60 * 60 * 1000, // 1 hour
-    limit: 10,
-    standardHeaders: 'draft-7',
-    legacyHeaders: false,
-    keyGenerator: (req) => getClientIp(req),
-    message: 'Too many requests, please try again later.',
-  }),
-);
+// export const logsRateLimiter = initMiddleware(
+//   rateLimit({
+//     windowMs: 60 * 60 * 1000, // 1 hour
+//     limit: 10,
+//     standardHeaders: 'draft-7',
+//     legacyHeaders: false,
+//     keyGenerator: (req) => getClientIp(req),
+//     message: 'Too many requests, please try again later.',
+//   }),
+// );
+export const logsRateLimiter = rateLimit({
+  windowMs: 60 * 60 * 1000, // 1 hour
+  limit: 10,
+  standardHeaders: 'draft-7',
+  legacyHeaders: false,
+  keyGenerator: (req: any) => getClientIp(req),
+  message: 'Too many requests, please try again later.',
+  store:
+    process.env.NODE_ENV === 'production'
+      ? new RedisStore({
+          // @ts-expect-error - Known issue: the `call` function is not present in @types/ioredis
+          sendCommand: async (...args: string[]) => new RedisClient({ host: process.env.REDIS_HOST }).call(...args),
+        })
+      : null,
+});
