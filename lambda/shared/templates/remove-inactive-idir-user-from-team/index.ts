a470220dda42b05c20289fc87b08f7a74dc0f8f7
jonathan langlois <jonathan.langlois@gov.bc.ca>
2025-05-30 15:16:19 -0700

    chore: snapshot test
    update snapshot test with new email

--- lambda/shared/templates/remove-inactive-idir-user-from-team/index.ts
+++ lambda/shared/templates/remove-inactive-idir-user-from-team/index.ts
@@ -5,11 +5,11 @@ import { EMAILS } from '@lambda-shared/enums';
 import type { RenderResult } from '../index';
 import { SSO_EMAIL_ADDRESS } from '@lambda-shared/local';
 
-const SUBJECT_TEMPLATE = `In-active IDIR User Removed`;
+const SUBJECT_TEMPLATE = `In-active IDIR User Removed From Team`;
 const template = fs.readFileSync(__dirname + '/remove-inactive-idir-user-from-team.html', 'utf8');
 
-const subjectHandler = Handlebars.compile(SUBJECT_TEMPLATE, { noEscape: true });
 const bodyHandler = Handlebars.compile(template, { noEscape: true });
+const subjectHandler = Handlebars.compile(SUBJECT_TEMPLATE, { noEscape: true });
 
 export const render = async (originalData: any): Promise<RenderResult> => {
   const data = { ...originalData };
@@ -20,9 +20,9 @@ export const render = async (originalData: any): Promise<RenderResult> => {
 };
 
 export const send = async (data: { emails: string[]; username: string; teamName: string }, rendered: RenderResult) => {
-  const { username, teamName, emails } = data;
+  const { emails } = data;
   return sendEmail({
-    code: EMAILS.DELETE_INACTIVE_IDIR_USER,
+    code: EMAILS.REMOVE_INACTIVE_IDIR_USER_FROM_TEAM,
     to: emails,
     cc: [SSO_EMAIL_ADDRESS],
     ...rendered,
