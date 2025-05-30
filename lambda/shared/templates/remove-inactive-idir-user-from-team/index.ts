92df11679fbb76483f422c5f8d19811dc944219e
Jonathan Langlois <37274633+jlangy@users.noreply.github.com>
2025-05-30 16:34:31 -0700

    chore: deleted user emails (#1435)
    * chore: disable iframe
    
    iframe causing more trouble than its worth cross-browser
    
    * chore: user deletion emails
    
    update email logic on inactive idir user deletions
    
    * chore: iframe testing
    
    revert iframe testing changes
    
    * chore: snapshot test
    
    update snapshot test with new email
    
    * chore: sonar recomendation
    
    implement reccomendation from sonarcloud

--- lambda/shared/templates/remove-inactive-idir-user-from-team/index.ts
+++ lambda/shared/templates/remove-inactive-idir-user-from-team/index.ts
@@ -4,13 +4,12 @@ import { sendEmail } from '@lambda-shared/utils/ches';
 import { EMAILS } from '@lambda-shared/enums';
 import type { RenderResult } from '../index';
 import { SSO_EMAIL_ADDRESS } from '@lambda-shared/local';
-import { getIntegrationEmails } from '../helpers';
 
-const SUBJECT_TEMPLATE = `In-active IDIR User Removed`;
-const template = fs.readFileSync(__dirname + '/delete-inactive-idir-users.html', 'utf8');
+const SUBJECT_TEMPLATE = `In-active IDIR User Removed From Team`;
+const template = fs.readFileSync(__dirname + '/remove-inactive-idir-user-from-team.html', 'utf8');
 
-const subjectHandler = Handlebars.compile(SUBJECT_TEMPLATE, { noEscape: true });
 const bodyHandler = Handlebars.compile(template, { noEscape: true });
+const subjectHandler = Handlebars.compile(SUBJECT_TEMPLATE, { noEscape: true });
 
 export const render = async (originalData: any): Promise<RenderResult> => {
   const data = { ...originalData };
@@ -20,10 +19,10 @@ export const render = async (originalData: any): Promise<RenderResult> => {
   };
 };
 
-export const send = async (data: any, rendered: RenderResult) => {
-  const emails = await getIntegrationEmails(data.integration);
+export const send = async (data: { emails: string[]; username: string; teamName: string }, rendered: RenderResult) => {
+  const { emails } = data;
   return sendEmail({
-    code: EMAILS.DELETE_INACTIVE_IDIR_USER,
+    code: EMAILS.REMOVE_INACTIVE_IDIR_USER_FROM_TEAM,
     to: emails,
     cc: [SSO_EMAIL_ADDRESS],
     ...rendered,
