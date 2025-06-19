4eec3b2ab8346d5a8a9006662d36affe60ce65de
Jonathan Sharman <jon.w.sharman@gmail.com>
2025-06-19 13:46:24 -0700

    chore: point local dev at correct terraform modules

--- local/terraform/main.tf
+++ local/terraform/main.tf
@@ -13,7 +13,7 @@ locals {
 }
 
 module "standard" {
-  source       = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/base-realms/realm-standard"
+  source       = "github.com/bcgov/sso-terraform?ref=dev/terraform-modules/modules/base-realms/realm-standard"
   keycloak_url = var.keycloak_url
 
   standard_realm_name      = local.standard_realm_name
