f34f4ed15580a347de1b4584260db7dbe64e7d0b
jonathan langlois <jonathan.langlois@gov.bc.ca>
2025-07-22 14:26:40 -0700

    chore: emails
    remove commented code

--- app/shared/local.ts
+++ app/shared/local.ts
@@ -18,20 +18,3 @@ export const {
     DIT_EMAIL_ADDRESS = "",
     DIT_ADDITIONAL_EMAIL_ADDRESS = "",
 } = parsedAddresses as any;
-
-// export const SSO_EMAIL_ADDRESS = 'bcgov.sso@gov.bc.ca';
-
-// export const IDIM_EMAIL_ADDRESS = app_env === 'production' ? 'idim.consulting@gov.bc.ca' : 'bcgov.sso@gov.bc.ca';
-
-// export const OTP_EMAIL_ADDRESS_CC =
-//   app_env === 'production' ? ['dt.support@gov.bc.ca', 'ditrust@gov.bc.ca'] : ['bcgov.sso@gov.bc.ca'];
-// export const OTP_EMAIL_ADDRESS_BCC =
-//   app_env === 'production' ? ['jonathan-langlois@live.ca'] : ['jonathan-langlois@live.ca'];
-
-// export const OCIO_EMAIL_ADDRESS = 'bcgov.sso@gov.bc.ca';
-
-// export const SOCIAL_APPROVAL_EMAIL_ADDRESS = 'bcgov.sso@gov.bc.ca';
-
-// export const DIT_EMAIL_ADDRESS = 'ditrust@gov.bc.ca';
-
-// export const DIT_ADDITIONAL_EMAIL_ADDRESS = app_env === 'production' ? 'aaron.unger@gov.bc.ca' : 'bcgov.sso@gov.bc.ca';
