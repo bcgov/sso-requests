6dcadf4e4b444583fc0555eeb92cb6aa486e6667
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2025-07-24 16:58:58 -0700

    feat: cache the octokit instance

--- app/utils/github.ts
+++ app/utils/github.ts
@@ -5,15 +5,21 @@ import getConfig from 'next/config';
 const { serverRuntimeConfig = {} } = getConfig() || {};
 const { gh_app_id, gh_app_installation_id, gh_app_private_key } = serverRuntimeConfig;
 
+let cachedOctokit: Octokit | null = null;
+
 export const authenticateToGithub = async () => {
-  const octokit = new Octokit({
-    authStrategy: createAppAuth,
-    auth: {
-      appId: gh_app_id,
-      privateKey: Buffer.from(gh_app_private_key, 'base64').toString('utf-8'),
-      installationId: gh_app_installation_id,
-    },
-  });
-  await octokit.rest.apps.getAuthenticated();
-  return octokit;
+  if (!cachedOctokit) {
+    cachedOctokit = new Octokit({
+      authStrategy: createAppAuth,
+      auth: {
+        appId: gh_app_id,
+        privateKey: Buffer.from(gh_app_private_key, 'base64').toString('utf-8'),
+        installationId: gh_app_installation_id,
+      },
+    });
+
+    await cachedOctokit.rest.apps.getAuthenticated();
+  }
+
+  return cachedOctokit;
 };
