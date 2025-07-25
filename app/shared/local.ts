deba7e3110031dfc955fd7c2b52702c5bb01b9d4
Jonathan Langlois <37274633+jlangy@users.noreply.github.com>
2025-07-25 11:41:36 -0700

    chore: hardcode emails (#1470)
    revert to hardcoded email addresses instead of environment variable

--- app/shared/local.ts
+++ app/shared/local.ts
@@ -1,20 +1,20 @@
-const { EMAIL_ADDRESSES } = process.env;
+import getConfig from 'next/config';
 
-let parsedAddresses = {};
+const { publicRuntimeConfig = {} } = getConfig() || {};
+const { app_env } = publicRuntimeConfig;
 
-try {
-  parsedAddresses = JSON.parse(EMAIL_ADDRESSES || '{}');
-} catch {
-  parsedAddresses = {};
-}
+export const SSO_EMAIL_ADDRESS = 'bcgov.sso@gov.bc.ca';
 
-export const {
-  SSO_EMAIL_ADDRESS = '',
-  IDIM_EMAIL_ADDRESS = '',
-  OTP_EMAIL_ADDRESS_CC = '',
-  OTP_EMAIL_ADDRESS_BCC = '',
-  OCIO_EMAIL_ADDRESS = '',
-  SOCIAL_APPROVAL_EMAIL_ADDRESS = '',
-  DIT_EMAIL_ADDRESS = '',
-  DIT_ADDITIONAL_EMAIL_ADDRESS = '',
-} = parsedAddresses as any;
+export const IDIM_EMAIL_ADDRESS = app_env === 'production' ? 'idim.consulting@gov.bc.ca' : 'bcgov.sso@gov.bc.ca';
+
+export const OCIO_EMAIL_ADDRESS = 'bcgov.sso@gov.bc.ca';
+
+export const SOCIAL_APPROVAL_EMAIL_ADDRESS = 'bcgov.sso@gov.bc.ca';
+
+export const DIT_EMAIL_ADDRESS = 'ditrust@gov.bc.ca';
+
+export const DIT_ADDITIONAL_EMAIL_ADDRESS = app_env === 'production' ? 'aaron.unger@gov.bc.ca' : 'bcgov.sso@gov.bc.ca';
+
+export const OTP_EMAIL_ADDRESS_CC =
+  app_env === 'production' ? ['ditrust@gov.bc.ca', 'dt.support@gov.bc.ca'] : ['bcgov.sso@gov.bc.ca'];
+export const OTP_EMAIL_ADDRESS_BCC = app_env === 'production' ? ['olena.mitovska@gov.bc.ca'] : ['bcgov.sso@gov.bc.ca'];
