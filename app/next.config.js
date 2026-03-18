e8ba501b1286b09a72e94520be9dd31f6021f56a
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2026-03-18 14:09:29 -0700

    fix: set turbopack root folder

--- app/next.config.js
+++ app/next.config.js
@@ -1,12 +1,18 @@
-import { resolve } from 'path';
+const path = require('path');
+const APP_URL = process.env.NEXT_PUBLIC_APP_URL || '';
+const BASE_PATH = process.env.APP_BASE_PATH || '';
 
-export const reactStrictMode = true;
-export const assetPrefix = process.env.NEXT_PUBLIC_APP_URL || '';
-export const basePath = process.env.APP_BASE_PATH || '';
-export const turbopack = {
-  resolveAlias: {
-    'pg-native': resolve(__dirname, './empty.ts'),
-    'cloudflare:sockets': resolve(__dirname, './empty.ts'),
+module.exports = {
+  reactStrictMode: true,
+  assetPrefix: APP_URL,
+  // basePath has to start with a /
+  // basePath has to be either an empty string or a path prefix
+  basePath: BASE_PATH,
+  turbopack: {
+    resolveAlias: {
+      'pg-native': path.resolve(__dirname, './empty.ts'),
+      'cloudflare:sockets': path.resolve(__dirname, './empty.ts'),
+    },
   },
-  root: resolve(__dirname, '..'),
+  root: path.resolve(__dirname, '..'),
 };
