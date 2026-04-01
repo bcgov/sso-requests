0f2466ba3a6b917bb568c68e811abd97fdc83b52
arcshiftsolutions <arcshiftsolutions@gmail.com>
2026-04-01 13:32:02 -0700

    feat: small change to outage page

--- app/error-messages/maintenance.tsx
+++ app/error-messages/maintenance.tsx
@@ -1,25 +1,13 @@
 export function getMaintenanceTemplate() {
-  const title = "We'll be back soon";
+  const title = "SSO is currently in recovery mode";
   const content = (
     <text transform="translate(228 245)" fill="#777" fontSize="18" fontFamily="OpenSans, Open Sans">
       <tspan x="0" y="0">
-        The CSS app is currently in maintenance mode.
+        The CSS app is unavailable at this time.
       </tspan>
       <tspan x="0" y="26">
-        You can check the status of both the app and Keycloak at:{' '}
+        Please try again later once the SSO application has recovered.
       </tspan>
-      <tspan y="52" x="0" fill="#006fc4">
-        <a href="https://chat.developer.gov.bc.ca/channel/sso" target="_blank" title="Rocket Chat" rel="noreferrer">
-          Rocket.Chat
-        </a>
-      </tspan>
-      <tspan y="52"> or by sending our team an </tspan>
-      <tspan y="52" fill="#006fc4">
-        <a href="mailto:bcgov.sso@gov.bc.ca" title="Pathfinder SSO" target="_blank" rel="noreferrer">
-          e-mail
-        </a>
-      </tspan>
-      <tspan y="26">.</tspan>
     </text>
   );
 
