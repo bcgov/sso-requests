32a1459be7903d4e667ff672080cad3bce09dd0a
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2026-07-27 18:34:52 -0700

    fix: api unit tests

--- app/queries/custom-requests.ts
+++ app/queries/custom-requests.ts
@@ -1,10 +1,10 @@
 import { models } from '@app/shared/sequelize/models/models';
-import { Op } from 'sequelize/lib/operators';
+import { Op } from 'sequelize';
 
 export const doSkipPrivacyZoneScope = async (requestId: number) => {
   const customRequest = await models.customRequest.findOne({
     where: {
-      requestId: requestId,
+      requestId,
       conditions: {
         [Op.contains]: { skipPrivacyZoneScope: true },
       },
