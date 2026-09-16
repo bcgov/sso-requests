7bdf4122b7217298981b696216a553905d752487
Nithin Shekar Kuruba <NithinShekar.Kuruba@gov.bc.ca>
2026-09-16 10:51:22 -0700

    fix: jest setup for api tests

--- app/jest-api/jest.setup.js
+++ app/jest-api/jest.setup.js
@@ -12,10 +12,10 @@ process.env.LOCAL_DEV = true;
 process.env.VERIFY_USER_SECRET = 'test';
 
 // Run integration sagas inline so API specs observe the final outcome within the request call.
-process.env.SAGA_EXECUTION_MODE = 'synchronous';
-process.env.SAGA_MAX_STEP_ATTEMPTS = '1';
-process.env.SAGA_RETRY_BASE_DELAY_MS = '0';
-process.env.SAGA_RETRY_MAX_DELAY_MS = '0';
+process.env.WORKFLOW_EXECUTION_MODE = 'synchronous';
+process.env.WORKFLOW_MAX_STEP_ATTEMPTS = '1';
+process.env.WORKFLOW_RETRY_BASE_DELAY_MS = '0';
+process.env.WORKFLOW_RETRY_MAX_DELAY_MS = '0';
 
 beforeAll(async () => {
   await cleanUpDatabaseTables();
