c0738fd6d8805df0d787cac0e8ecfb224532e6f8
Nithin Shekar Kuruba <NithinShekar.Kuruba@gov.bc.ca>
2026-09-17 10:59:11 -0700

    feat: update host in cypress config

--- app/cypress.config.ts
+++ app/cypress.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
     json: true,
   },
   e2e: {
-    baseUrl: 'http://sso-requests-sandbox.apps.gold.devops.gov.bc.ca/',
+    baseUrl: 'https://sso-requests-sandbox.apps.gold.devops.gov.bc.ca/',
     projectId: 'gctfmh',
     experimentalModifyObstructiveThirdPartyCode: true,
     setupNodeEvents(on, config) {
